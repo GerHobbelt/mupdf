90b684766e6d4793da0c69131b56c4d6ab199f03
Tor Andersson <tor.andersson@artifex.com>
2020-09-24 11:58:18 +0200

    fixup! graftPage

--- platform/java/mupdf_native.h
+++ platform/java/mupdf_native.h
@@ -1978,6 +1978,14 @@ JNIEXPORT jobject JNICALL Java_com_artifex_mupdf_fitz_PDFDocument_graftObject
 
 /*
  * Class:     com_artifex_mupdf_fitz_PDFDocument
+ * Method:    graftPage
+ * Signature: (ILcom/artifex/mupdf/fitz/PDFDocument;I)V
+ */
+JNIEXPORT void JNICALL Java_com_artifex_mupdf_fitz_PDFDocument_graftPage
+  (JNIEnv *, jobject, jint, jobject, jint);
+
+/*
+ * Class:     com_artifex_mupdf_fitz_PDFDocument
  * Method:    addStreamBuffer
  * Signature: (Lcom/artifex/mupdf/fitz/Buffer;Ljava/lang/Object;Z)Lcom/artifex/mupdf/fitz/PDFObject;
  */
