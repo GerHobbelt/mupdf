a8814ffd079ca6a239ad5859ecab6d222d823455
Ger Hobbelt <ger@hobbelt.com>
2020-10-06 01:19:59 +0200

    - provide the major tools and DLL with fz_open_url() and fz_open_file_progressive() APIs. - make sure functions fz_open_file_progressive() and fz_open_url() are always interpreted as C functions.

--- platform/x11/curl_stream.h
+++ platform/x11/curl_stream.h
@@ -1,7 +1,17 @@
 #ifndef CURL_STREAM_H
 #define CURL_STREAM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 fz_stream *fz_open_file_progressive(fz_context *ctx, const char *filename, int kbps,
 	void (*on_data)(void*,int), void *arg);
 fz_stream *fz_open_url(fz_context *ctx, const char *url, int kbps,
 	void (*on_data)(void*,int), void *arg);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
