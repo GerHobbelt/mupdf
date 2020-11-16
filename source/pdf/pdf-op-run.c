484518458cee175fe7a7bcf2a9cca52ac6afc05e
Tor Andersson <tor.andersson@artifex.com>
2020-11-16 14:15:33 +0100

    WIP better warnings when characters can't be encoded

--- source/pdf/pdf-op-run.c
+++ source/pdf/pdf-op-run.c
@@ -950,7 +950,7 @@ show_string(fz_context *ctx, pdf_run_processor *pr, unsigned char *buf, size_t l
 		if (cid >= 0)
 			pdf_show_char(ctx, pr, cid);
 		else
-			fz_warn(ctx, "cannot encode character");
+			fz_warn(ctx, "cannot encode character 0x%x with cmap %s", cpt, fontdesc->encoding->cmap_name);
 		if (cpt == 32 && w == 1)
 		{
 			/* Bug 703151: pdf_show_char can realloc gstate. */
