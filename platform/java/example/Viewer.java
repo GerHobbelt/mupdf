e2de3f6d0001517fb17a6bc00edb84e3e6766750
Sebastian Rasmussen <sebras@gmail.com>
2020-12-23 18:00:19 +0100

    WIP: java: Rudimentary support for testing secure redactions.

--- platform/java/example/Viewer.java
+++ platform/java/example/Viewer.java
@@ -426,6 +426,11 @@ public class Viewer extends Frame implements WindowListener, ActionListener, Ite
 			clearSearch();
 	}
 
+	public void save() {
+		PDFDocument pdf = (PDFDocument) doc;
+		pdf.redactSaveSecure("/tmp/tomte.pdf", "");
+	}
+
 	protected void canvasKeyTyped(KeyEvent e) {
 		char c = e.getKeyChar();
 
@@ -433,6 +438,7 @@ public class Viewer extends Frame implements WindowListener, ActionListener, Ite
 		{
 		case 'r': reload(); break;
 		case 'q': dispose(); break;
+		case 'S': save(); break;
 
 		case 'f': toggleFullscreen(); break;
 
