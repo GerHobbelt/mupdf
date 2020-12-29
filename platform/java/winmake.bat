b7eb11fe5f4d76b2c9b2587ba279af775e19e590
Ger Hobbelt <ger@hobbelt.com>
2020-12-30 00:46:09 +0100

    preliminary tweaking of the java build batch file as the original one reported mutiple issues with the latest Java JDK.

--- platform/java/winmake.bat
+++ platform/java/winmake.bat
@@ -6,13 +6,13 @@ echo bogus > src\com\artifex\mupdf\fitz\bogus.class
 del /Q src\com\artifex\mupdf\fitz\*.class
 
 @echo Building Viewer
-javac -classpath src -sourcepath src -source 1.7 -target 1.7 example/Viewer.java
+javac -Xlint:deprecation -classpath src -sourcepath src example/Viewer.java
 
 @echo Building JNI classes
-javac -sourcepath src -source 1.7 -target 1.7 src/com/artifex/mupdf/fitz/*.java
+javac -Xlint:deprecation -sourcepath src src/com/artifex/mupdf/fitz/*.java
 
 @echo Importing DLL (built using VS solution)
-@copy ..\win32\%1\javaviewerlib.dll mupdf_java.dll /y
+if exist ..\win32\%1\javaviewerlib.dll ( @copy ..\win32\%1\javaviewerlib.dll mupdf_java.dll /y ) else ( @echo "Cannot find ..\win32\%1\javaviewerlib.dll" )
 
 @echo Packaging into jar (incomplete as missing manifest)
 jar cf mupdf-java-viewer.jar mupdf_java.dll src\com\artifex\mupdf\fitz\*.class example\*.class
