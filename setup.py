c3dcb49a6becbeec3642855ee09891d645902153
Julian Smith <julian.smith@artifex.com>
2021-03-16 17:07:04 +0000

    setup.py: use new pipcl.py

--- setup.py
+++ setup.py
@@ -6,6 +6,103 @@ Installation script for MuPDF Python bindings.
 For creating a sdist, we need to be a git checkout - we use 'git ls-files'.
 '''
 
+import os
+import sys
+
+
+if 1:
+    import re
+    import subprocess
+    import sys
+    import time
+
+    import pipcl
+
+    log = pipcl.log
+
+    log(f'== sys.argv={sys.argv}')
+
+    mupdf_dir = os.path.abspath(f'{__file__}/..')
+
+    def mupdf_version():
+        with open(f'{mupdf_dir}/include/mupdf/fitz/version.h') as f:
+            text = f.read()
+        m = re.search('\n#define FZ_VERSION "([^"]+)"\n', text)
+        assert m
+        version = m.group(1)
+        # Append unique(ish) suffix so we can easily experiment on test.pypi.org.
+        #
+        version += time.strftime(".%Y%m%d.%H%M")
+        return version
+
+    out_names = [
+            'libmupdf.so',      # C
+            'libmupdfcpp.so',   # C++
+            '_mupdf.so',        # Python internals
+            'mupdf.py',         # Python
+            ]
+
+    build_dir = 'build/shared-release'
+
+    def sdist():
+        return pipcl.git_items( '.', submodules=True)
+
+    def build():
+        if 0:
+            # Fake build.
+            command = 'true'
+            for name in names:
+                command += ( ' && rsync -ai'
+                        + f' /home/jules/artifex/mupdf/build/shared-release/{name}'
+                        + f' {mupdf_dir}/{build_dir}/{name}'
+                        )
+        else:
+            command = f'cd {mupdf_dir} && ./scripts/mupdfwrap.py -d {build_dir} -b all'
+
+        log(f'Building mupdf C, C++ and Python libraries with: {command}')
+        subprocess.check_call(command, shell=True)
+
+        pys = []
+        sos = []
+        datas = []
+        for name in out_names:
+            path = f'{mupdf_dir}/{build_dir}/{name}'
+            if path.endswith('.py'):
+                pys.append( (path, name))
+            elif path.endswith('.so'):
+                sos.append( (path, name))
+            else:
+                datas.append( (path, name))
+        return pys, sos, datas
+
+    pipcl.run(
+            argv = sys.argv,
+            name = 'mupdf',
+            version = mupdf_version(),
+            summary = 'Python bindings for MuPDF library',
+            description = 'Python bindings for MuPDF library',
+            classifiers = [
+                    'Development Status :: 4 - Beta',
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: GNU Affero General Public License v3',
+                    'Programming Language :: Python :: 3',
+                    ],
+            author = 'Artifex Software, Inc',
+            author_email = 'support@artifex.com',
+            url_docs = 'https://twiki.ghostscript.com/do/view/Main/MuPDFWrap/',
+            url_home = 'https://mupdf.com/',
+            url_source = 'https://git.ghostscript.com/?p=mupdf.git',
+            url_tracker = 'https://bugs.ghostscript.com/',
+            keywords = 'PDF',
+            platform = None,
+            fn_clean = None,
+            fn_sdist = sdist,
+            fn_build = build,
+            )
+
+    sys.exit()
+
+
 import io
 import os
 import re
@@ -16,6 +113,17 @@ import sys
 import tarfile
 import time
 import zipfile
+
+def mupdf_version():
+    with open(f'{mupdf_dir}/include/mupdf/fitz/version.h') as f:
+        text = f.read()
+    m = re.search('\n#define FZ_VERSION "([^"]+)"\n', text)
+    assert m
+    version = m.group(1)
+    # Append unique(ish) suffix so we can easily experiment on test.pypi.org.
+    #
+    version += time.strftime(".%Y%m%d.%H%M")
+    return version
 
 def log(text=''):
     '''
@@ -29,16 +137,6 @@ log(f'== sys.argv={sys.argv}')
 
 mupdf_dir = os.path.abspath(f'{__file__}/..')
 
-def mupdf_version():
-    with open(f'{mupdf_dir}/include/mupdf/fitz/version.h') as f:
-        text = f.read()
-    m = re.search('\n#define FZ_VERSION "([^"]+)"\n', text)
-    assert m
-    version = m.group(1)
-    # Append unique(ish) suffix so we can easily experiment on test.pypi.org.
-    #
-    version += time.strftime(".%Y%m%d.%H%M")
-    return version
 
 
 # Information about supported build directories.
