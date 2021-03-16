a0aa41298b1114f47b50d64a76abac4bd909846e
Julian Smith <julian.smith@artifex.com>
2021-03-16 17:06:04 +0000

    pipcl.py: support for writing setup.py scripts.

---
+++ pipcl.py
@@ -0,0 +1,351 @@
+'''
+Support for implementing a setup.py package script.
+'''
+
+import io
+import os
+import shutil
+import site
+import subprocess
+import sys
+import tarfile
+
+
+def log(text=''):
+    '''
+    Logs lines with prefix.
+    '''
+    for line in text.split('\n'):
+        print(f'pipcl.py: {line}', file=sys.stdout)
+    sys.stdout.flush()
+
+
+def git_items( directory, submodules=False):
+    '''
+    Helper for run()'s fn_sdist() callback.
+
+    <directory> must be somewhere within a git checkout.
+
+    Returns list of paths for all files known to git within <directory>. Each
+    path starts with <directory>.
+
+    We use the 'git ls-files' command.
+    '''
+    assert os.path.isdir( '%s/.git' % directory)
+    command = 'cd ' + directory + ' && git ls-files'
+    if submodules:
+        command += ' --recurse-submodules'
+    text = subprocess.check_output( command, shell=True)
+    ret = []
+    for path in text.decode('utf8').strip().split( '\n'):
+        path2 = os.path.join(directory, path)
+        if os.path.isdir(path2):
+            log(f'*** Ignoring git ls-files item that is actually a directory: {path2}')
+        else:
+            ret.append(path2)
+    return ret
+
+
+def run(
+        argv,
+        name,
+        version,
+        summary = None,
+        description = None,
+        classifiers = None,
+        author = None,
+        author_email = None,
+        url_docs = None,
+        url_home = None,
+        url_source = None,
+        url_tracker = None,
+        keywords = None,
+        platform = None,
+        fn_clean = None,
+        fn_sdist = None,
+        fn_build = None,
+        ):
+    '''
+    Handles command-line passed to setup.py, for Python package specified by
+    the args.
+
+    argv
+        E.g. sys.argv. argv[0] is ignored.
+    name
+        A string, the name of the Python package.
+    version
+        A string containing only 0-9 and '.'.
+    summary
+        A string.
+    description
+        A string.
+    classifiers
+        List of strings.
+    url_home
+    url_source
+    url_docs
+    utl_tracker
+        Useful URLs.
+    keywords
+        A string containing space-separated keywords, or a list of keywords.
+    platform
+        A string.
+    fn_clean
+        A function taking no args that cleans generated files.
+    fn_sdist
+        A function taking no args, returning a list of paths, e.g. from
+        git_items(), for files that should be copied into the sdist.
+    fn_build
+        A function taking no args that builds everything.
+
+        Should return (pys, sos, datas), each a list of items, each item being
+        either a string <path> or a tuple (from_, to_). <path> is treated as a
+        tuple (path, path).
+
+            pys: .py files. For each item, we copy from_ to
+            .../site-packages/<to_>.
+
+            sos: .so files. For each item, we copy <from_> to
+            .../site-packages/<to_>.
+
+            datas: misc files. For each item, we copy <from_> to
+            .../site-packages/<to_>.
+
+        E.g. fn_build() could return:
+
+                (
+                    [
+                        ('build/foo/__init__.py', 'foo/__init__.py'),
+                        ('build/bar.py', 'bar.py'),
+                        'wibble.py',
+                    ],
+                    [
+                        ('build/objs/_wibble.so', '_wibble.so'),
+                    ],
+                    [
+                        ('src/data.txt', 'data/data.txt'),
+                    ]
+                )
+
+        And this could result in the installed package looking like:
+
+            .../site-packages/
+                foo/__init__.py
+                bar.py
+                wibble.py
+                _wibble.so
+                data/data.txt
+    '''
+
+    class ArgsRaise:
+        pass
+
+    class Args:
+        '''
+        Iterates over argv items.
+        '''
+        def __init__( self, argv):
+            self.items = iter( argv)
+        class Raise:
+            pass
+        def next( self, eof=ArgsRaise):
+            '''
+            Returns next arg. If no more args, we return eof or raise an exception
+            if eof == ArgsRaise.
+            '''
+            try:
+                return next( self.items)
+            except StopIteration:
+                if eof == ArgsRaise:
+                    raise Exception('Not enough args')
+                return eof
+
+    def metainfo():
+        ret = ['']
+        def add(key, value):
+            if value is not None:
+                ret[0] += f'{key}: {value}\n'
+        add('Metadata-Version', '1.2')
+        add('Name', name)
+        add('Version', version)
+        add('Summary', summary)
+        add('Home-page', url_home)
+        add('Platform', platform)
+        add('Author', author)
+        add('Author-email', author_email)
+        if url_source:  add('Home-page', f'Source, {url_source}')
+        if url_docs:    add('Home-page', f'Source, {url_docs}')
+        if url_tracker: add('Home-page', f'Source, {url_tracker}')
+        if keywords:
+            keywords2 = keywords
+            if isinstance(keywords2, str):
+                keywords2 = keywords2.split()
+            keywords2 = ' '.join(keywords2)
+            add('Keywords', keywords2)
+        if classifiers:
+            classifiers2 = classifiers
+            if isinstance(classifiers2, str):
+                classifiers2 = classifiers2.split('\n')
+            for classifier in classifiers2:
+                add('Classifier', classifier)
+        return ret[0]
+
+    command = None
+
+    opt_egg_base = None
+    opt_install_headers = None
+    opt_record = None
+    opt_d = None
+    opt_dist_dir = 'dist'
+
+    args = Args(argv[1:])
+
+    while 1:
+        arg = args.next(None)
+        if arg is None:
+            break
+
+        elif arg in ('clean','egg_info', 'install', 'sdist'):
+            assert command is None, 'Two commands specified: {command} and {arg}.'
+            command = arg
+
+        elif arg == '--compile':
+            pass
+
+        elif arg == '-d':
+            opt_d = args.next()
+
+        elif arg == '--dist-dir':
+            opt_dist_dir = args.next()
+
+        elif arg == '--egg-base':
+            opt_egg_base = args.next()
+
+        elif arg in ('-h', '--help'):
+            log(
+                    'Options:\n'
+                    '    clean\n'
+                    '        Not implemented.\n'
+                    '    egg_info\n'
+                    '        Creates files in: <egg-base>/.egg-info/\n'
+                    '    install\n'
+                    '        Installs into Python\'s site.getsitepackages()[0].\n'
+                    '        Logs to <record> (not implemented).\n'
+                    '    sdist\n'
+                    '        Make a source distribution:\n'
+                    '            <dist-dir>/<name>-<version>.tar.gz\n'
+                    '    --egg-base <egg-base>\n'
+                    '        Not implemented.\n'
+                    '    -d <d>\n'
+                    '        Not implemented.\n'
+                    '    --dist-dir <dist-dir>\n'
+                    '        Default is "dist".'
+                    '    --record <record>\n'
+                    '        Ignored.\n'
+                    '    --single-version-externally-managed\n'
+                    '        Ignored.\n'
+                    '    --compile\n'
+                    '        Ignored.\n'
+                    '    --install-headers <directory>\n'
+                    '        Ignored.\n'
+                    )
+            return
+
+        elif arg == '--help-commands':
+            print('help')
+            return
+
+        elif arg == '--install-headers':
+            opt_install_headers = args.next()
+
+        elif arg == '--record':
+            # todo: do something with this, presumably write a log to the
+            # specified file?
+            #
+            opt_record = args.next()
+
+        elif arg == '--single-version-externally-managed':
+            pass
+
+        else:
+           raise Exception(f'Unrecognised arg: {arg}')
+
+    if not command:
+        return
+
+    log(f'Handling command={command}')
+    if 0:
+        pass
+
+    elif command == 'clean':
+        if fn_clean:
+            fn_clean()
+
+    elif command == 'egg_info':
+        assert opt_egg_base
+        root = f'{opt_egg_base}/.egg-info'
+        os.mkdir(root)
+        with open(f'{root}/PKG-INFO', 'w') as f:
+            f.write(metainfo())
+        with open(f'{root}/SOURCES.txt', 'w') as f:
+            pass
+        with open(f'{root}/dependency_links.txt', 'w') as f:
+            pass
+        with open(f'{root}/top_level.txt', 'w') as f:
+            f.write(f'{name}\n')
+
+    elif command == 'install':
+        if fn_build:
+            pys, sos, datas = fn_build()
+
+        sitepackages = site.getsitepackages()[0]
+
+        # If we create a <opt_record> file, pip complains that we haven't
+        # indicated that we've installed an .egg-info directory. So for now we
+        # ignore <opt_record>.
+        #
+        f = 0   #open(opt_record, 'w') if opt_record else None
+        try:
+            # For now, we copy everything into .../site-packages/.
+            for item in pys + sos + datas:
+                if isinstance(item, str):
+                    from_ = item
+                    to_ = item
+                else:
+                    from_, to_ = item
+                to_path = f'{sitepackages}/{to_}'
+                log( f'Copying {from_} to {to_path}')
+                shutil.copy2( from_, f'{to_path}')
+                if f:
+                    f.write(f'{to_path}\n')
+        finally:
+            if f:
+                f.close()
+
+    elif command == 'sdist':
+        if fn_sdist:
+            paths = fn_sdist()
+        else:
+            paths = []
+        shutil.rmtree(f'{opt_dist_dir}', ignore_errors=True)
+        os.mkdir(f'{opt_dist_dir}')
+
+        def add(tar, name, contents):
+            '''
+            Adds item called <name> to tarfile.TarInfo <tar>, containing
+            <contents> (which must be bytes, not str).
+            '''
+            ti = tarfile.TarInfo(name)
+            ti.size = len(contents)
+            tar.addfile(ti, io.BytesIO(contents))
+
+        tarpath = f'{opt_dist_dir}/{name}-{version}.tar.gz'
+        with tarfile.open(tarpath, 'w:gz') as tar:
+            for path in paths:
+                tar.add( path, path, recursive=False)
+            add(tar, 'PKG-INFO', metainfo().encode('utf8'))
+
+        log( f'Have created: {tarpath}')
+
+    else:
+        assert 0, f'Unrecognised command: {command}'
