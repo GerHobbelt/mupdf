6a1ea3d756122511f83ce095fcf3ca398f1d3c5e
Julian Smith <julian.smith@artifex.com>
2021-03-22 11:59:42 +0000

    Added python packaging support.
    scripts/pipcl.py
        Generic python packaging support, translates argv from pip to callbacks in
        setup.py.
    
    setup.py
    
        New, python packaging support.
    
        Allow generation of Python wheel on systems without clang-python and/or
        swig.
    
        When generating a sdist, we run './scripts/mupdfwrap.py -b 0' to build
        C++ source and include this source in the sdist. We also include some new
        .pickle files which contain additional C++ information used when generating
        python binding.
    
        When generating a wheel, we allow control over whether to use clang-python
        and/or swig.
    
        If $MUPDF_SETUP_HAVE_CLANG_PYTHON is set but not '1', we don't generate C++
        code, instead we expect it to be already in place.
    
        If MUPDF_SETUP_HAVE_SWIG is set but not '1', we don't run SWIG to generate
        C++ code, instead expecting it to already exist.

---
+++ scripts/pipcl.py
@@ -0,0 +1,450 @@
+'''
+Support for implementing a setup.py package script.
+'''
+
+import base64
+import hashlib
+import io
+import os
+import shutil
+import site
+import subprocess
+import sys
+import tarfile
+import time
+import zipfile
+
+
+def log(text=''):
+    '''
+    Logs lines with prefix.
+    '''
+    for line in text.split('\n'):
+        print(f'pipcl.py: {line}')
+    sys.stdout.flush()
+
+
+def git_items( directory, submodules=False):
+    '''
+    Helper for run()'s fn_sdist() callback.
+
+    <directory> must be somewhere within a git checkout.
+
+    Returns list of paths for all files known to git within <directory>. Each
+    path starts with <directory>.
+
+    We use the 'git ls-files' command.
+    '''
+    assert os.path.isdir( '%s/.git' % directory)
+    command = 'cd ' + directory + ' && git ls-files'
+    if submodules:
+        command += ' --recurse-submodules'
+    text = subprocess.check_output( command, shell=True)
+    ret = []
+    for path in text.decode('utf8').strip().split( '\n'):
+        path2 = os.path.join(directory, path)
+        if os.path.isdir(path2):
+            log(f'*** Ignoring git ls-files item that is actually a directory: {path2}')
+        else:
+            ret.append(path2)
+    return ret
+
+
+def run(
+        argv,
+        name,
+        version,
+        summary = None,
+        description = None,
+        classifiers = None,
+        author = None,
+        author_email = None,
+        url_docs = None,
+        url_home = None,
+        url_source = None,
+        url_tracker = None,
+        keywords = None,
+        platform = None,
+        fn_clean = None,
+        fn_sdist = None,
+        fn_build = None,
+        license_files = None,
+        ):
+    '''
+    Handles command-line argv passed to setup.py.
+
+    The args we are passed define the Python package that we work with.
+
+    argv
+        E.g. sys.argv. argv[0] is ignored.
+    name
+        A string, the name of the Python package.
+    version
+        A string containing only 0-9 and '.'.
+    summary
+        A string.
+    description
+        A string.
+    classifiers
+        A list of strings.
+    url_home
+    url_source
+    url_docs
+    utl_tracker
+        Strings containing particular URLs.
+    keywords
+        A string containing space-separated keywords, or a list of keywords.
+    platform
+        A string, used in metainfo.
+    fn_clean
+        A function taking a single arg <all_> that cleans generated files.
+        <all_> is true iff --all is in argv.
+    fn_sdist
+        A function taking no args that returns a list of paths, e.g. from
+        git_items(), for files that should be copied into the sdist.
+    fn_build
+        A function taking no args that builds everything.
+
+        Should return a list of items, each item being a tuple of two strings
+        (from_, to_), or a single string <path> which is treated as a tuple
+        (path, path).
+
+        <from_> should be the path to a file. <to_> identifies what the file
+        should be called when generating a wheel or installing.
+
+        The 'bdist_wheel' command will copy <from_> to <to_> in the wheel.
+
+        The 'install' command will copy <from_> to <sitepackages>/<to_> where
+        <sitepackages> is Python's site.getsitepackages()[0].
+
+    license_files
+        Either string name of license file or list of names of license files.
+    '''
+
+    class ArgsRaise:
+        pass
+
+    class Args:
+        '''
+        Iterates over argv items.
+        '''
+        def __init__( self, argv):
+            self.items = iter( argv)
+        def next( self, eof=ArgsRaise):
+            '''
+            Returns next arg. If no more args, we return eof or raise an exception
+            if eof == ArgsRaise.
+            '''
+            try:
+                return next( self.items)
+            except StopIteration:
+                if eof == ArgsRaise:
+                    raise Exception('Not enough args')
+                return eof
+
+    def metainfo():
+        '''
+        Returns text for .egg-info/PKG-INFO file, or PKG-INFO in an sdist
+        tar.gz file, or ...dist-info/METADATA in a wheel.
+        '''
+        # We use an array here so that local add() function can modify ret[0].
+        #
+        ret = ['']
+        def add(key, value):
+            if value is not None:
+                ret[0] += f'{key}: {value}\n'
+        add('Metadata-Version', '1.2')
+        add('Name', name)
+        add('Version', version)
+        add('Summary', summary)
+        add('Home-page', url_home)
+        add('Platform', platform)
+        add('Author', author)
+        add('Author-email', author_email)
+        if url_source:  add('Home-page', f'Source, {url_source}')
+        if url_docs:    add('Home-page', f'Source, {url_docs}')
+        if url_tracker: add('Home-page', f'Source, {url_tracker}')
+        if keywords:
+            keywords2 = keywords
+            if isinstance(keywords2, str):
+                keywords2 = keywords2.split()
+            keywords2 = ' '.join(keywords2)
+            add('Keywords', keywords2)
+        if classifiers:
+            classifiers2 = classifiers
+            if isinstance(classifiers2, str):
+                classifiers2 = classifiers2.split('\n')
+            for classifier in classifiers2:
+                add('Classifier', classifier)
+        return ret[0]
+
+    command = None
+
+    opt_all = None
+    opt_dist_dir = 'dist'
+    opt_egg_base = None
+    opt_install_headers = None
+    opt_record = None
+
+    args = Args(argv[1:])
+
+    while 1:
+        arg = args.next(None)
+        if arg is None:
+            break
+
+        elif arg in ('bdist_wheel', 'clean', 'egg_info', 'install', 'sdist'):
+            assert command is None, 'Two commands specified: {command} and {arg}.'
+            command = arg
+
+        elif arg == '--all':
+            opt_all = True
+
+        elif arg == '--compile':
+            pass
+
+        elif arg == '--dist-dir' or arg == '-d':
+            opt_dist_dir = args.next()
+
+        elif arg == '--egg-base':
+            opt_egg_base = args.next()
+
+        elif arg in ('-h', '--help'):
+            log(
+                    'Options:\n'
+                    '    bdist_wheel\n'
+                    '        Creates a wheel called\n'
+                    '        <dist-dir>/<name>-<version>-<details>.whl, where\n'
+                    '        <dist-dir> is "dist" or as specified by --dist-dir,\n'
+                    '        and <details> encodes ABI and platform etc.\n'
+                    '    clean\n'
+                    '        Cleans build files.\n'
+                    '    egg_info\n'
+                    '        Creates files in: <egg-base>/.egg-info/, where\n'
+                    '        <egg-base> is as specified with --egg-base.\n'
+                    '    install\n'
+                    '        Installs into Python\'s\n'
+                    '        site.getsitepackages()[0]. Writes installation\n'
+                    '        information to <record> if --record\n'
+                    '        was specified.\n'
+                    '    sdist\n'
+                    '        Make a source distribution:\n'
+                    '            <dist-dir>/<name>-<version>.tar.gz\n'
+                    '    --dist-dir | -d <dist-dir>\n'
+                    '        Default is "dist".\n'
+                    '    --egg-base <egg-base>\n'
+                    '        Used by "egg_info".\n'
+                    '    --record <record>\n'
+                    '        Used by "install".\n'
+                    '    --single-version-externally-managed\n'
+                    '        Ignored.\n'
+                    '    --compile\n'
+                    '        Ignored.\n'
+                    '    --install-headers <directory>\n'
+                    '        Ignored.\n'
+                    )
+            return
+
+        elif arg == '--help-commands':
+            print('help')
+            return
+
+        elif arg == '--install-headers':
+            opt_install_headers = args.next()
+
+        elif arg == '--record':
+            # todo: do something with this, presumably write a log to the
+            # specified file?
+            #
+            opt_record = args.next()
+
+        elif arg == '--single-version-externally-managed':
+            pass
+
+        else:
+           raise Exception(f'Unrecognised arg: {arg}')
+
+    if not command:
+        return
+
+    def fromto(p):
+        if isinstance(p, str):
+            return p, p
+        elif len(p) == 2:
+            from_, to_ = p
+            if isinstance(from_, str) and isinstance(to_, str):
+                return from_, to_
+        assert 0, 'p should be str or (str, str), but is: {p}'
+
+    log(f'Handling command={command}')
+    if 0:
+        pass
+
+    elif command == 'bdist_wheel':
+
+        os.makedirs(opt_dist_dir, exist_ok=True)
+
+        items = []
+        if fn_build:
+            items = fn_build()
+
+        path = f'{opt_dist_dir}/{name}-{version}-py3-none-any.whl'
+        log(f'creating wheel/zip: {path}')
+
+        record = Record()
+        with zipfile.ZipFile(path, 'w') as z:
+
+            def add_file(from_, to_):
+                z.write(from_, to_)
+                record.add_file(from_, to_)
+
+            def add_str(content, to_):
+                z.writestr(to_, content)
+                record.add_content(content, to_)
+
+            # Add the files returned by fn_build().
+            #
+            for item in items:
+                from_, to_ = fromto(item)
+                add_file(from_, to_)
+
+            # Add ...dist-info/*.
+            #
+            d = f'{name}-{version}.dist-info'
+            content = (''
+                    + 'Wheel-Version: 1.0\n'
+                    + 'Generator: bdist_wheel\n'
+                    + 'Root-Is-Purelib: false\n'
+                    + 'Tag: py3-none-any\n'
+                    )
+            add_str(content, f'{d}/WHEEL')
+            add_str(metainfo(), f'{d}/METADATA')
+
+            # Add license files.
+            #
+            if license_files:
+                if isinstance(license_files, str):
+                    license_files = license_files,
+                for l in license_files:
+                    from_, to_ = fromto(l)
+                    add_file(from_, f'{d}/{to_}')
+
+            # Update ...dist-info/RECORD. This must be last.
+            #
+            #log(f'RECORD is:\n{record[0]}')
+            z.writestr(f'{d}/RECORD', record.get())
+
+    elif command == 'clean':
+        if fn_clean:
+            fn_clean(opt_all)
+
+    elif command == 'egg_info':
+        assert opt_egg_base
+        root = f'{opt_egg_base}/.egg-info'
+        log(f'{command}: creating files in root={root}')
+        os.mkdir(root)
+        with open(f'{root}/PKG-INFO', 'w') as f:
+            f.write(metainfo())
+        with open(f'{root}/SOURCES.txt', 'w') as f:
+            pass
+        with open(f'{root}/dependency_links.txt', 'w') as f:
+            pass
+        with open(f'{root}/top_level.txt', 'w') as f:
+            f.write(f'{name}\n')
+
+    elif command == 'install':
+        items = []
+        if fn_build:
+            items = fn_build()
+
+        # Find where to install.
+        #
+        # It seems that some items don't exist, so we can't simply use
+        # site.getsitepackages()[0].
+        #
+        sitepackages_all = site.getsitepackages()
+        for p in sitepackages_all:
+            if os.path.exists(p):
+                sitepackages = p
+                break
+        else:
+            text = 'No item exists in site.getsitepackages():\n'
+            for i in sitepackages_all:
+                text += f'    {i}\n'
+            raise Exception(text)
+
+        record = Record() if opt_record else None
+        for item in items:
+            from_, to_ = fromto(item)
+            to_path = f'{sitepackages}/{to_}'
+            log(f'copying from {from_} to {to_path}')
+            shutil.copy2( from_, f'{to_path}')
+            if record:
+                # Could maybe use relative path of to_path from sitepackages/.
+                record.add_file(from_, to_)
+
+        if opt_record:
+            with open(opt_record, 'w') as f:
+                f.write(record.get())
+
+    elif command == 'sdist':
+        if fn_sdist:
+            paths = fn_sdist()
+        else:
+            paths = []
+        shutil.rmtree(f'{opt_dist_dir}', ignore_errors=True)
+        os.mkdir(f'{opt_dist_dir}')
+
+        def add(tar, name, contents):
+            '''
+            Adds item called <name> to tarfile.TarInfo <tar>, containing
+            <contents> (which must be bytes, not str).
+            '''
+            ti = tarfile.TarInfo(name)
+            ti.size = len(contents)
+            ti.mtime = time.time()
+            tar.addfile(ti, io.BytesIO(contents))
+
+        tarpath = f'{opt_dist_dir}/{name}-{version}.tar.gz'
+        with tarfile.open(tarpath, 'w:gz') as tar:
+            for path in paths:
+                if os.path.abspath(path).startswith(f'{os.path.abspath(opt_dist_dir)}/'):
+                    # Ignore files inside <opt_dist_dir>, to save fn_sdist()
+                    # from worrying about this.
+                    continue
+                if not os.path.exists(path):
+                    # This appears to happen for sub-submodules.
+                    log(f'Ignoring non-existant path: {path}')
+                    continue
+                #log(f'path={path}')
+                tar.add( path, path, recursive=False)
+            add(tar, 'PKG-INFO', metainfo().encode('utf8'))
+
+        log( f'Have created: {tarpath}')
+
+    else:
+        assert 0, f'Unrecognised command: {command}'
+
+
+class Record:
+    '''
+    Builds up text suitable for writing to a RECORD item, e.g. within a wheel.
+    '''
+    def __init__(self):
+        self.text = ''
+
+    def add_content(self, content, to_):
+        if isinstance(content, str):
+            content = content.encode('latin1')
+        h = hashlib.sha256(content)
+        digest = h.digest()
+        digest = base64.urlsafe_b64encode(digest)
+        self.text += f'{to_},sha256={digest},{len(content)}\n'
+
+    def add_file(self, from_, to_):
+        with open(from_, 'rb') as f:
+            content = f.read()
+        self.add_content(content, to_)
+
+    def get(self):
+        return self.text
---
+++ setup.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+
+'''
+Installation script for MuPDF Python bindings, using scripts/pipcl.py.
+
+Default behaviour:
+
+    When building an sdist (e.g. with 'pip sdist'), we use clang-python to
+    generate C++ source which is then included in the sdist.
+
+    When installing or building a wheel (e.g. 'pip install <sdist>'), we
+    assume that generated C++ source is already present. Thus we don't rely on
+    clang-python being present.
+
+Extra command-line options:
+
+    --mupdf-build 0 | 1 | <path>
+        0:  We do not do a build.
+        1:  We do a build.
+        <path>:
+            Instead of doing a build we copy from an existing build directory
+            called <path>, e.g. from a different tree with: --mupdf-build
+            .../mupdf/build/shared-release
+
+        If --mupdf-build is not specified we use $MUPDF_SETUP_BUILD if it is
+        set.
+
+        Otherwise we default to "--mupdf-build 1".
+
+    --mupdf-build-dir <build-dir>
+        Sets the type of build by specifing the build directory, for example:
+        --mupdf-build-dir build/shared-debug
+
+Environmental variables:
+
+    MUPDF_SETUP_BUILD_DIR
+        Sets the build directory; overriden if --mupdf-build-dir is specified.
+
+    MUPDF_SETUP_HAVE_CLANG_PYTHON
+        Affects whether we use clang-python when building.
+
+        If set and not '1', we do not attempt to use clang-python, and instead
+        assume that generated files are already available in platform/c++/.
+
+        Default is '0'.
+
+    MUPDF_SETUP_HAVE_SWIG
+        If set and not '1', we do not attempt to run swig, and instead assume
+        that generated files are already available in platform/python/.
+'''
+
+import os
+import re
+import subprocess
+import sys
+import sys
+import time
+
+
+def log(text=''):
+    for line in text.split('\n'):
+        print(f'setup.py: {line}')
+    sys.stdout.flush()
+
+mupdf_dir = os.path.abspath(f'{__file__}/..')
+
+log(f'== sys.argv={sys.argv}')
+log(f'== mupdf_dir={mupdf_dir}')
+
+sys.path.append(f'{mupdf_dir}/scripts')
+import pipcl
+
+
+def mupdf_version():
+    with open(f'{mupdf_dir}/include/mupdf/fitz/version.h') as f:
+        text = f.read()
+    m = re.search('\n#define FZ_VERSION "([^"]+)"\n', text)
+    assert m
+    version = m.group(1)
+    # Append unique(ish) suffix so we can easily experiment on test.pypi.org
+    # and make multiple Python releases without requiring an increment to the
+    # MuPDF version.
+    #
+    version += time.strftime(".%Y%m%d.%H%M")
+    return version
+
+# Generated files that are copied into the Python installation.
+#
+out_names = [
+        'libmupdf.so',      # C
+        'libmupdfcpp.so',   # C++
+        '_mupdf.so',        # Python internals
+        'mupdf.py',         # Python
+        ]
+
+build_dir = os.environ.get('MUPDF_SETUP_BUILD_DIR', 'build/shared-release')
+log(f'MUPDF_SETUP_BUILD_DIR={os.environ.get("MUPDF_SETUP_BUILD_DIR")}')
+log(f'build_dir={build_dir}')
+
+mupdf_build = None
+
+# Process any of our own command-line options.
+#
+i = 1
+while 1:
+    if i == len(sys.argv):
+        break
+    if sys.argv[i] == '--mupdf-build':
+        mupdf_build = sys.argv[i+1]
+        del sys.argv[i:i+2]
+    elif sys.argv[i] == '--mupdf-build-dir':
+        build_dir = sys.argv[i+1]
+        infix = 'build/shared-'
+        if infix not in build_dir:
+            raise Exception('--mupdf-build-dir value "{build_dir}" must contain {infix}')
+        del sys.argv[i:i+2]
+    else:
+        i += 1
+
+def sdist():
+    '''
+    pipcl callback. If we are a git checkout, return all files known to
+    git. Otherwise return all files except for those in build/.
+    '''
+    if not os.path.exists('.git'):
+        raise Exception(f'Cannot make sdist because not a git checkout')
+    paths = pipcl.git_items( '.', submodules=True)
+
+    # Build C++ files and SWIG C code for inclusion in sdist, so that it can be
+    # used on systems without clang-python or SWIG.
+    #
+    subprocess.check_call(f'./scripts/mupdfwrap.py -d {build_dir} -b 02', shell=True)
+    paths += [
+            'build/shared-release/mupdf.py',
+            'platform/c++/container_classnames.pickle',
+            'platform/c++/implementation/classes.cpp',
+            'platform/c++/implementation/exceptions.cpp',
+            'platform/c++/implementation/functions.cpp',
+            'platform/c++/implementation/internal.cpp',
+            'platform/c++/include/mupdf/classes.h',
+            'platform/c++/include/mupdf/exceptions.h',
+            'platform/c++/include/mupdf/functions.h',
+            'platform/c++/include/mupdf/internal.h',
+            'platform/c++/swig_c.pickle',
+            'platform/c++/swig_python.pickle',
+            'platform/c++/to_string_structnames.pickle',
+            'platform/python/mupdfcpp_swig.cpp',
+            ]
+    return paths
+
+def build():
+    '''
+    pipcl callback. Build MuPDF C, C++ and Python libraries, with exact
+    behaviour depending on <mupdf_build> and <build_dir>.
+    '''
+    global mupdf_build
+    if mupdf_build is None:
+        mupdf_build = os.environ.get('MUPDF_SETUP_BUILD')
+
+    if mupdf_build is None:
+        mupdf_build = '1'
+
+    if mupdf_build == '1':
+        have_clang_python = os.environ.get('MUPDF_SETUP_HAVE_CLANG_PYTHON', '0') == '1'
+        have_swig = os.environ.get('MUPDF_SETUP_HAVE_SWIG', '1') == '1'
+        b = 'm'         # Build C library.
+        if have_clang_python:
+            b += '0'    # Build C++ source.
+        b += '1'        # Build C++ library.
+        if have_swig:
+            b += '2'    # Build SWIG-generated source.
+        b += '3'        # Build SWIG library _mupdf.so.
+        command = f'cd {mupdf_dir} && ./scripts/mupdfwrap.py -d {build_dir} -b {b}'
+    elif mupdf_build == '0':
+        command = None
+    else:
+        log(f'build(): Building by copying files from {mupdf_build} into {build_dir}/')
+        command = f'mkdir -p {build_dir}'
+        for n in out_names:
+            command += f' && rsync -ai {mupdf_build}/{n} {build_dir}/'
+
+    if command:
+        log(f'build(): Building MuPDF C, C++ and Python libraries with: {command}')
+        subprocess.check_call(command, shell=True)
+    else:
+        log(f'build(): Not building mupdf because mupdf_build={mupdf_build}')
+
+    paths = []
+    for name in out_names:
+        path = f'{mupdf_dir}/{build_dir}/{name}'
+        paths.append((path, name))
+    log(f'build(): returning: {paths}')
+    return paths
+
+def clean(all_):
+    if all_:
+        subprocess.check_call('(rm -r build || true)', shell=True)
+    else:
+        subprocess.check_call(f'(rm -r {build_dir} || true)', shell=True)
+
+pipcl.run(
+        argv = sys.argv,
+        name = 'mupdf',
+        version = mupdf_version(),
+        summary = 'Python bindings for MuPDF library',
+        description = 'Python bindings for MuPDF library',
+        classifiers = [
+                'Development Status :: 4 - Beta',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: GNU Affero General Public License v3',
+                'Programming Language :: Python :: 3',
+                ],
+        author = 'Artifex Software, Inc',
+        author_email = 'support@artifex.com',
+        url_docs = 'https://twiki.ghostscript.com/do/view/Main/MuPDFWrap/',
+        url_home = 'https://mupdf.com/',
+        url_source = 'https://git.ghostscript.com/?p=mupdf.git',
+        url_tracker = 'https://bugs.ghostscript.com/',
+        keywords = 'PDF',
+        platform = None,
+        fn_clean = clean,
+        fn_sdist = sdist,
+        fn_build = build,
+        license_files = 'COPYING',
+        )
