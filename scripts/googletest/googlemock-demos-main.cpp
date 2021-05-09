7e666654d01478949a668e7773316d8ef07b81a5
Ger Hobbelt <ger@hobbelt.com>
2021-05-10 00:36:55 +0200

    the crazy link FATAL ERROR went away by setting 'generate browse info' to NO, i.e. 'removing' the /FR or /Fr commandline options.
    Diagnosis: we reduced the gmock demo project to the bare metal and the linker error disappeared then. Slowly adding the gmock source files one by one got us a 'BSCMAKE: internal limit reached' error with the added advice to kill /FR, /Fr options. So we did. Slowly adding the remainder of the gmock source files did not revert to the dreaded fatal linker error.
    
    Also taking the previous vcxproj state (which fails to link due to that weird linker error), removing the /FR options from there also *fixed* the linker failure problem, hence conclusion: don't generate BSC files or the linker may go b0rk b0rk b0rk. Very odd, but this is what we've got...
    
    --> removed /FR from all googletest project files.

--- scripts/googletest/googlemock-demos-main.cpp
+++ scripts/googletest/googlemock-demos-main.cpp
@@ -5,7 +5,6 @@
 int gmock_output_test_main(int argc, const char** argv);
 int gmock_test_main(int argc, const char** argv);
 int gmock_spec_builders_test_main(int argc, const char** argv);
-int gmock_spec_builders_test_main(int argc, const char** argv);
 int gmock_stress_test_main(int argc, const char** argv);
 
 int gtest_main(int argc, const char** argv);
@@ -16,7 +15,6 @@ int main(int argc, const char** argv)
 	int rv = 0;
 	rv |= gmock_output_test_main(argc, argv);
 	rv |= gmock_test_main(argc, argv);
-	rv |= gmock_spec_builders_test_main(argc, argv);
 	rv |= gmock_spec_builders_test_main(argc, argv);
 	rv |= gmock_stress_test_main(argc, argv);
 
