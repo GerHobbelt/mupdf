f627b885df6876fea07aebe707050bec28890f14
Ger Hobbelt <ger@hobbelt.com>
2021-05-17 13:24:05 +0200

    add provisional googletest unit test project -- to be used with our extension work on mutool et al.

---
+++ scripts/muq-unittest/main.cpp
@@ -0,0 +1,8 @@
+
+#include "pch.h"
+
+GTEST_API_ int main(int argc, const char** argv) {
+	printf("Running main() from %s\n", __FILE__);
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
