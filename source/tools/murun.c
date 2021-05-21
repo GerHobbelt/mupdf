990962d9919cfe3548ac117b712d2b671bb036c7
Sebastian Rasmussen <sebras@gmail.com>
2021-05-21 14:57:39 +0200

    fixup! js: Fix inklist parsing.

--- source/tools/murun.c
+++ source/tools/murun.c
@@ -5619,6 +5619,7 @@ static void ffi_PDFAnnotation_getInkList(js_State *J)
 			fz_catch(ctx)
 				rethrow(J);
 			ffi_pushpoint(J, pt);
+			js_setindex(J, -2, k);
 		}
 		js_setindex(J, -2, i);
 	}
@@ -5639,7 +5640,7 @@ static void ffi_PDFAnnotation_setInkList(js_State *J)
 	nv = 0;
 	for (i = 0; i < n; ++i) {
 		js_getindex(J, 1, i);
-		nv += js_getlength(J, -1) / 2;
+		nv += js_getlength(J, -1);
 		js_pop(J, 1);
 	}
 
@@ -5659,9 +5660,11 @@ static void ffi_PDFAnnotation_setInkList(js_State *J)
 	}
 	for (i = v = 0; i < n; ++i) {
 		js_getindex(J, 1, i);
-		counts[i] = js_getlength(J, -1) / 2;
+		counts[i] = js_getlength(J, -1);
 		for (k = 0; k < counts[i]; ++k) {
+			js_getindex(J, -1, k);
 			points[v] = ffi_topoint(J, -1);
+			js_pop(J, 1);
 			++v;
 		}
 		js_pop(J, 1);
@@ -5692,22 +5695,19 @@ static void ffi_PDFAnnotation_addInkList(js_State *J)
 {
 	fz_context *ctx = js_getcontext(J);
 	pdf_annot *annot = js_touserdata(J, 0, "pdf_annot");
-	int i, k, m, n = js_getlength(J, 1);
+	int i, n = js_getlength(J, 1);
+	fz_point pt;
 	fz_try(ctx)
 		pdf_add_annot_ink_list_stroke(ctx, annot);
 	fz_catch(ctx)
 		rethrow(J);
-	for (i = 0; i < n; i += 2) {
+	for (i = 0; i < n; ++i) {
 		js_getindex(J, 1, i);
-		m = js_getlength(J, -1);
-		for (k = 0; k < m; ++k) {
-			fz_point pt = ffi_topoint(J, -1);
-			fz_try(ctx)
-				pdf_add_annot_ink_list_stroke_vertex(ctx, annot, pt);
-			fz_catch(ctx)
-				rethrow(J);
-
-		}
+		pt = ffi_topoint(J, -1);
+		fz_try(ctx)
+			pdf_add_annot_ink_list_stroke_vertex(ctx, annot, pt);
+		fz_catch(ctx)
+			rethrow(J);
 		js_pop(J, 1);
 	}
 }
