6bcbc733116532c9a2f996157c1cb61781073b9e
Ger Hobbelt <ger@hobbelt.com>
2021-05-22 20:13:18 +0200

    got google::log to compile and test -- requirement for LDA.Familia library (and several others, e.g. LDA.big.ARTM) added google:log ("glog") unit test and examples to the muq-unittest monolithic test application.

---
+++ scripts/muq-unittest/test_glog.cpp
@@ -0,0 +1,89 @@
+#include "pch.h"
+
+#include "../../thirdparty/owemdjee/glog/src/testing.h"
+
+
+
+
+static void init_test_argv(int& argc, const char**& argv)
+{
+	static const char* test_argv[] = {
+		"unit-test",
+		NULL
+	};
+	argv = test_argv;
+	argc = int(sizeof(test_argv) / sizeof(test_argv[0])) - 1;
+}
+
+
+
+TEST(LibGoogleLog, demangle) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_demangle_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, logging_custom_prefix) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_logging_custom_prefix_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, logging_stripped) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_logging_striptest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, logging) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_logging_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, mock_log) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_mock_log_test_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, signalhandler) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_signalhandler_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, stacktrace) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_stacktrace_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, stl_logging) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_stl_logging_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, symbolize) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_symbolize_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
+TEST(LibGoogleLog, utilities) {
+	int lcl_argc;
+	const char** lcl_argv;
+	init_test_argv(lcl_argc, lcl_argv);
+	EXPECT_EQ(glog_utilities_unittest_main(lcl_argc, lcl_argv), EXIT_SUCCESS);
+}
+
