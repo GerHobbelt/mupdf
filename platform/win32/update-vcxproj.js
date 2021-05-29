fc64892577bd5780ab89839918f28474832a3669
Ger Hobbelt <ger@hobbelt.com>
2021-05-29 19:04:08 +0200

    - copied pthread-win32 and cryptopp projects to solution directory, so we don't bugger the MSVC projects that came with those repos. - executed the update-vcxproj.js update script to ensure all projects not have the EXACT SAME compiler, etc. settings for all build modes.

--- platform/win32/update-vcxproj.js
+++ platform/win32/update-vcxproj.js
@@ -53,6 +53,129 @@ src = src
 //    <IntDir>$(SolutionDir)obj\$(Configuration)-$(CharacterSet)-$(PlatformArchitecture)bit-$(PlatformShortname)\$(RootNamespace)-$(ConfigurationType)-$(ProjectName)\</IntDir>
 .replace(/<IntDir>[^<]+<\/IntDir>/g, (m) => `<IntDir>$(SolutionDir)obj\\$(Configuration)-$(CharacterSet)-$(PlatformArchitecture)bit-$(PlatformShortname)\\$(RootNamespace)-$(ConfigurationType)-$(ProjectName)\\</IntDir>`)
 
+/*
+    <ClCompile>
+      <Optimization>Disabled</Optimization>
+      <AdditionalIncludeDirectories>.;../../thirdparty/owemdjee/clipp/include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <PreprocessorDefinitions>BUILD_MONOLITHIC;BUILDING_LIBCLIPP;CLIPP_STATICLIB;_CRT_SECURE_NO_WARNINGS;_CRTDBG_MAP_ALLOC;WIN32;_DEBUG;_WINDOWS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <MinimalRebuild>false</MinimalRebuild>
+      <BasicRuntimeChecks>EnableFastChecks</BasicRuntimeChecks>
+      <RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>
+      <PrecompiledHeaderOutputFile>$(IntDir)$(TargetName).pch</PrecompiledHeaderOutputFile>
+      <AssemblerListingLocation>$(IntDir)</AssemblerListingLocation>
+      <ObjectFileName>$(IntDir)</ObjectFileName>
+      <ProgramDataBaseFileName>$(IntDir)$(ProjectName).pdb</ProgramDataBaseFileName>
+      <BrowseInformation>false</BrowseInformation>
+      <WarningLevel>Level3</WarningLevel>
+      <SuppressStartupBanner>true</SuppressStartupBanner>
+      <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <DisableSpecificWarnings>4244;4018;4244;4267;5105</DisableSpecificWarnings>
+      <LanguageStandard>stdcpp17</LanguageStandard>
+      <LanguageStandard_C>stdc17</LanguageStandard_C>
+      <SupportJustMyCode>false</SupportJustMyCode>
+      <SDLCheck>false</SDLCheck>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <StringPooling>true</StringPooling>
+      <ExceptionHandling>Async</ExceptionHandling>
+      <EnableEnhancedInstructionSet>AdvancedVectorExtensions2</EnableEnhancedInstructionSet>
+      <FloatingPointModel>Fast</FloatingPointModel>
+      <ConformanceMode>true</ConformanceMode>
+    </ClCompile>
+*/    
+
+let compiler_settings = `
+<ItemDefinitionGroup>
+    <ClCompile>
+      <Optimization>Disabled</Optimization>
+      <MinimalRebuild>true</MinimalRebuild>
+      <BasicRuntimeChecks>EnableFastChecks</BasicRuntimeChecks>
+      <RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>
+      <PrecompiledHeaderOutputFile>$(IntDir)$(TargetName).pch</PrecompiledHeaderOutputFile>
+      <AssemblerListingLocation>$(IntDir)</AssemblerListingLocation>
+      <ObjectFileName>$(IntDir)</ObjectFileName>
+      <ProgramDataBaseFileName>$(IntDir)$(ProjectName).pdb</ProgramDataBaseFileName>
+      <BrowseInformation>false</BrowseInformation>
+      <WarningLevel>Level3</WarningLevel>
+      <SuppressStartupBanner>true</SuppressStartupBanner>
+      <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <LanguageStandard>stdcpp17</LanguageStandard>
+      <LanguageStandard_C>stdc17</LanguageStandard_C>
+      <SupportJustMyCode>false</SupportJustMyCode>
+      <SDLCheck>false</SDLCheck>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <StringPooling>true</StringPooling>
+      <ExceptionHandling>Async</ExceptionHandling>
+      <EnableEnhancedInstructionSet>AdvancedVectorExtensions2</EnableEnhancedInstructionSet>
+      <FloatingPointModel>Fast</FloatingPointModel>
+      <EnableFiberSafeOptimizations>true</EnableFiberSafeOptimizations>
+      <FloatingPointExceptions>true</FloatingPointExceptions>
+      <ConformanceMode>true</ConformanceMode>
+    </ClCompile>
+</ItemDefinitionGroup>
+`;
+		
+src = src
+.replace(/<ItemDefinitionGroup([^>]*)>([\s\S]*?)<\/ItemDefinitionGroup>/g, (m, p1, p2) => {
+	let isDebug = p1.length == 0 || /Debug/.test(p1);
+
+	// make sure these entries are always present:
+	if (!/<Optimization>/.test(p2)) {
+		p2 = p2.replace(/<ClCompile>/, `<ClCompile>
+			<Optimization>XXX</Optimization>`);
+	}
+
+	if (!/<BasicRuntimeChecks>/.test(p2)) {
+		p2 = p2.replace(/<ClCompile>/, `<ClCompile>
+			<BasicRuntimeChecks>XXX</BasicRuntimeChecks>`);
+	}
+
+	if (!/<RuntimeLibrary>/.test(p2)) {
+		p2 = p2.replace(/<ClCompile>/, `<ClCompile>
+			<RuntimeLibrary>XXX</RuntimeLibrary>`);
+	}
+
+	// now patch the entries and remove those that all build modees have in common:
+	p2 = p2
+	.replace(/<Optimization>[^<]+<\/Optimization>/g, (m) => `<Optimization>${ isDebug ? "Disabled" : "MaxSpeed" }</Optimization>`)
+	.replace(/<MinimalRebuild>[^<]+<\/MinimalRebuild>/g, (m) => ``)
+	.replace(/<BasicRuntimeChecks>[^<]+<\/BasicRuntimeChecks>/g, (m) => `<BasicRuntimeChecks>${ isDebug ? "EnableFastChecks" : "Default" }</BasicRuntimeChecks>`)
+	.replace(/<RuntimeLibrary>[^<]+<\/RuntimeLibrary>/g, (m) => `<RuntimeLibrary>${ isDebug ? "MultiThreadedDebugDLL" : "MultiThreadedDLL" }</RuntimeLibrary>`)
+	.replace(/<AssemblerListingLocation>[^<]+<\/AssemblerListingLocation>/g, (m) => ``)
+	.replace(/<ObjectFileName>[^<]+<\/ObjectFileName>/g, (m) => ``)
+	.replace(/<ProgramDataBaseFileName>[^<]+<\/ProgramDataBaseFileName>/g, (m) => ``)
+	.replace(/<BrowseInformation>[^<]+<\/BrowseInformation>/g, (m) => ``)
+	.replace(/<WarningLevel>[^<]+<\/WarningLevel>/g, (m) => ``)
+	.replace(/<SuppressStartupBanner>[^<]+<\/SuppressStartupBanner>/g, (m) => ``)
+	.replace(/<DebugInformationFormat>[^<]+<\/DebugInformationFormat>/g, (m) => ``)
+	.replace(/<FunctionLevelLinking>[^<]+<\/FunctionLevelLinking>/g, (m) => ``)
+	.replace(/<LanguageStandard>[^<]+<\/LanguageStandard>/g, (m) => ``)
+	.replace(/<LanguageStandard_C>[^<]+<\/LanguageStandard_C>/g, (m) => ``)
+	.replace(/<SupportJustMyCode>[^<]+<\/SupportJustMyCode>/g, (m) => ``)
+	.replace(/<SDLCheck>[^<]+<\/SDLCheck>/g, (m) => ``)
+	.replace(/<MultiProcessorCompilation>[^<]+<\/MultiProcessorCompilation>/g, (m) => ``)
+	.replace(/<IntrinsicFunctions>[^<]+<\/IntrinsicFunctions>/g, (m) => ``)
+	.replace(/<StringPooling>[^<]+<\/StringPooling>/g, (m) => ``)
+	.replace(/<ExceptionHandling>[^<]+<\/ExceptionHandling>/g, (m) => ``)
+	.replace(/<EnableEnhancedInstructionSet>[^<]+<\/EnableEnhancedInstructionSet>/g, (m) => ``)
+	.replace(/<FloatingPointModel>[^<]+<\/FloatingPointModel>/g, (m) => ``)
+	.replace(/<EnableFiberSafeOptimizations>[^<]+<\/EnableFiberSafeOptimizations>/g, (m) => ``)
+	.replace(/<FloatingPointExceptions>[^<]+<\/FloatingPointExceptions>/g, (m) => ``)
+	.replace(/<ConformanceMode>[^<]+<\/ConformanceMode>/g, (m) => ``)
+
+
+	return `<ItemDefinitionGroup${p1}>${p2}</ItemDefinitionGroup>`;
+});
+
+
+// prepend this common blob before all other blobs:
+src = src
+.replace(/<ItemDefinitionGroup/, (m) => {
+	return `${compiler_settings}${m}`;
+});
 
 fs.writeFileSync(filepath, src, 'utf8');
 
