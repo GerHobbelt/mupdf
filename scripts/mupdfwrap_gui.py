dda298d0096665ce308c5daf686f19b6d6506261
Julian Smith <julian.smith@artifex.com>
2021-07-13 10:29:54 +0100

    scripts/mupdfwrap_gui.py: can show pdf as bitmap.

---
+++ scripts/mupdfwrap_gui.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+'''
+Example usage:
+    PYTHONPATH=build/shared-debug-extract ./scripts/mupdfwrap_gui.py
+'''
+
+import mupdf
+
+if 0:
+    import tkinter
+
+    class App(tkinter.Frame):
+        def __init__(self, master=None):
+            super().__init__(master)
+            self.pack()
+
+    app = App()
+
+    app.master.title("MuPDF tkinter app")
+    app.mainloop()
+
+import PyQt5
+import PyQt5.QtWidgets
+
+
+print(f'{PyQt5.__file__}')
+
+g_pixmaps = []
+
+def open_pdf(path):
+    document = mupdf.Document(path)
+    page = mupdf.Page(document, 0)
+    ctm = mupdf.Matrix(1, 0, 0, 1, 0, 0)
+    colorspace = mupdf.Colorspace(mupdf.Colorspace.Fixed_RGB)
+    alpha = 0
+    pixmap = page.new_pixmap_from_page_contents(ctm, colorspace, alpha)
+    g_pixmaps.append(pixmap)
+    image = PyQt5.QtGui.QImage(
+            int(pixmap.pixmap_samples()),
+            pixmap.pixmap_width(),
+            pixmap.pixmap_height(),
+            PyQt5.QtGui.QImage.Format_RGB888,
+            );
+    return image
+
+open_pdf('zlib.clean.pdf')
+
+class MainWindow(PyQt5.QtWidgets.QMainWindow):
+    pass
+
+print(dir(PyQt5))
+app = PyQt5.QtWidgets.QApplication([])
+label = PyQt5.QtWidgets.QLabel('Hello world')
+label.show()
+
+g_document = [None]
+
+window = MainWindow()
+menu = window.menuBar().addMenu("&File")
+open_ = PyQt5.QtWidgets.QAction("&Open")
+
+def open_fn():
+    path = PyQt5.QtWidgets.QFileDialog.getOpenFileName(window, "Open", filter="*.pdf")[0]
+    if 0 and path:
+        document = mupdf.Document(path)
+        #output = mupdf.Output()
+        #writer = mupdf.DocumentWriter(output, '', mupdf.DocumentWriter.
+        page = mupdf.Page(document, 0)
+        ctm = mupdf.Matrix(1, 0, 0, 1, 0, 0)
+        colorspace = mupdf.Colorspace()
+        alpha = 0
+        pixmap = page.new_pixmap_from_page_contents(ctm, colorspace, alpha)
+    if 1:
+        image = open_pdf(path)
+        #label = PyQt5.QtWidgets.QLabel(path)
+        qpixmap = PyQt5.QtGui.QPixmap.fromImage(image)
+        print(f'calling label.setImage')
+        label.setPixmap(qpixmap)
+
+open_.triggered.connect(open_fn)
+menu.addAction(open_)
+
+window.show()
+app.exec_()
