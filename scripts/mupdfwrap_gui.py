68a01ce4a0ae764fda883c4d7b53cdaeeb608593
Julian Smith <julian.smith@artifex.com>
2021-07-14 12:56:34 +0100

    scripts/mupdfwrap_gui.py: added support for showing html. various other things.

--- scripts/mupdfwrap_gui.py
+++ scripts/mupdfwrap_gui.py
@@ -34,11 +34,15 @@ class MainWindow(PyQt5.QtWidgets.QMainWindow):
         self.menu_file_open = PyQt5.QtWidgets.QAction('&Open')
         self.menu_file_open.triggered.connect(self.open_)
 
+        self.menu_file_show_html = PyQt5.QtWidgets.QAction('&Show html')
+        self.menu_file_show_html.triggered.connect(self.show_html)
+
         self.menu_file_quit = PyQt5.QtWidgets.QAction('&Quit')
         self.menu_file_quit.triggered.connect(self.quit)
 
         menu_file = self.menuBar().addMenu('&File')
         menu_file.addAction(self.menu_file_open)
+        menu_file.addAction(self.menu_file_show_html)
         menu_file.addAction(self.menu_file_quit)
 
     def keyPressEvent(self, event):
@@ -62,6 +66,24 @@ class MainWindow(PyQt5.QtWidgets.QMainWindow):
         #print(f'resizeEvent(): oldSize={event.oldSize()} size={event.size()}')
         #print(f'minimumSize()={self.minimumSize()}')
 
+    def show_html(self):
+        if 1:
+            buffer_ = self.page.document_data_from_page("docx", "html", mupdf.Matrix(1, 0, 0, 1, 0, 0), mupdf.Cookie())
+        else:
+            buffer_ = mupdf.Buffer(0)
+            output = mupdf.Output(buffer_)
+            docx_writer = mupdf.DocumentWriter(output, "docx", "html")
+            device = docx_writer.begin_page(self.page.bound_page())
+            self.page.run_page(device, mupdf.Matrix(1, 0, 0, 1, 0, 0), mupdf.Cookie())
+            docx_writer.end_page()
+            docx_writer.close_document_writer()
+
+        html_content = buffer_.buffer_extract().decode('utf8')
+        #print(f'buffer_.buffer_extract() => {html_content}')
+        self.webview = PyQt5.QtWebKitWidgets.QWebView()
+        self.webview.setHtml(html_content)
+        self.webview.show()
+
     def open_(self):
         path = PyQt5.QtWidgets.QFileDialog.getOpenFileName(self, 'Open', filter='*.pdf')[0]
         if path:
@@ -76,20 +98,24 @@ class MainWindow(PyQt5.QtWidgets.QMainWindow):
         if zoom is None:
             zoom = self.zoom
         try:
-            page = mupdf.Page(self.document, page_number)
+            self.page = mupdf.Page(self.document, page_number)
         except Exception as e:
             print(f'Cannot go to page {page_number}: {e}')
             return
-        page_rect = page.bound_page()
+        page_rect = self.page.bound_page()
         z = 2**(zoom / self.zoom_multiple)
         # Using -2 here avoids always-present horizontal scrollbar; not sure
         # why...
         z *= (self.centralWidget().size().width() - 2) / (page_rect.x1 - page_rect.x0)
-        self.pixmap = page.new_pixmap_from_page_contents(
-                ctm=mupdf.Matrix(z, 0, 0, z, 0, 0),
-                cs=mupdf.Colorspace(mupdf.Colorspace.Fixed_RGB),
-                alpha=0,
-                )
+        try:
+            self.pixmap = self.page.new_pixmap_from_page_contents(
+                    ctm=mupdf.Matrix(z, 0, 0, z, 0, 0),
+                    cs=mupdf.Colorspace(mupdf.Colorspace.Fixed_RGB),
+                    alpha=0,
+                    )
+        except Exception as e:
+            print(f'self.page.new_pixmap_from_page_contents() failed: {e}')
+            return
         # Need to preserve <pixmap> after we return because <image> will refer to
         # it.
         #print(f'pixmap.pixmap_width()={pixmap.pixmap_width()} pixmap.pixmap_height()={pixmap.pixmap_height()} pixmap.pixmap_stride()={pixmap.pixmap_stride()}')
@@ -104,10 +130,6 @@ class MainWindow(PyQt5.QtWidgets.QMainWindow):
         self.central_widget.setPixmap(qpixmap)
         self.page_number = page_number
         self.zoom = zoom
-        # It seems that minimum size defaults to show all of central widget,
-        # which prevents resizing by the user to a smaller size.
-        #
-        self.setMinimumSize(10, 10)
 
     def open_path(self, path):
         try:
@@ -122,7 +144,22 @@ app = PyQt5.QtWidgets.QApplication([])
 
 main_window = MainWindow()
 
-main_window.open_path('zlib.clean.pdf')
+args = iter(sys.argv[1:])
+while 1:
+    try:
+        arg = next(args)
+    except StopIteration:
+        break
+    if arg.startswith('-'):
+        if arg == '--html':
+            main_window.show_html()
+        else:
+            raise Exception(f'Unrecognised arg: {arg}')
+    else:
+        main_window.open_path(arg)
+
+#main_window.open_path('zlib.clean.pdf')
+#main_window.show_html()
 
 main_window.show()
 app.exec_()
