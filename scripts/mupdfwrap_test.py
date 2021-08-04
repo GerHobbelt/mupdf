14543f20719e0fe2f68bd82ce515d6f500f0a8d5
Julian Smith <julian.smith@artifex.com>
2021-08-05 00:18:10 +0100

    scripts/: Fixed constructors of DocumentWriter - wrapper class for fz_document_writer.
    Added support for constructing DocumentWriter instances from several
    fz_new_*_writer() and fz_new_*_writer_with_output() functions that were
    previously omitted.
    
    Some constructors need to take an enum to differentiate between fz_ functions
    that take identical types; have renamed these enums for consistency and added
    som new enums.
    
    Also changed constructors that wrap fz_new_*_writer_with_output()
    functions to take a non-const 'Output& out' and set out.m_internal
    to NULL before returning. This is necessary because the underlying
    fz_new_*_writer_with_output() functions take ownership of the fz_output, which
    does not support reference counting.
    
    Patched up scripts/mupdfwrap_test.py and scripts/mutool.py to use the new
    constructors.

--- scripts/mupdfwrap_test.py
+++ scripts/mupdfwrap_test.py
@@ -254,6 +254,7 @@ def test(path):
     log(f'{type(stdout)} {stdout.m_internal.state}')
 
     mediabox = page.bound_page()
+<<<<<<< HEAD
     out = mupdf.DocumentWriter(filename, 'png', '', mupdf.DocumentWriter.FormatPathType_DOCUMENT)
     dev = out.begin_page(mediabox)
     page.run_page(dev, mupdf.Matrix(mupdf.fz_identity), mupdf.Cookie())
