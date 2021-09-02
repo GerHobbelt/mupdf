4758ff8fd915eba94ec9230d62780857d905db10
Ger Hobbelt <ger@hobbelt.com>
2021-09-02 12:54:10 +0200

    tweak the tesseract build configuration to better compiler configuration

--- scripts/tesseract/config_auto.h
+++ scripts/tesseract/config_auto.h
@@ -8,20 +8,31 @@
 // defined: support image sizes > 32K*32K pixels.
 #define LARGE_IMAGES 1
 
-
-#define __AVX2__                         1
-#define __AVX__                          1
+// https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros?redirectedfrom=MSDN&view=msvc-160
+//
+//#define __AVX2__                         1
+//#define __AVX__                          1
+#if _M_IX86_FP > 0
 #define __FMA__                          1
 #define __SSE4_1__                       1
+#endif
 
+#ifdef __AVX2__
 #define HAVE_AVX2                        1
+#endif
+#ifdef __AVX__
 #define HAVE_AVX                         1
+#endif
+#ifdef __FMA__
 #define HAVE_FMA                         1
+#endif
 #define HAVE_LEPTONICA                   1
 #define HAVE_LIBCURL                     1
 #define CURL_STATICLIB                   1
 #define HAVE_MUPDF                       1
+#ifdef __SSE4_1__
 #define HAVE_SSE4_1                      1
+#endif
 #define HAVE_TESSERACT                   1
 #define HAVE_TIFFIO_H                    1
 
