737e2048eda2221dac314f378ea576860060d25c
Ger Hobbelt <ger@hobbelt.com>
2021-09-03 19:09:55 +0200

    added version fetch&patch script; now only used for jpeg-XL but any other library that needs this type of work can be added later.
    Uses the tips found at https://stackoverflow.com/questions/1937702/visual-studio-run-c-project-post-build-event-even-if-project-is-up-to-date

---
+++ scripts/libjpeg-xl/tool_version_git.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#define JPEGXL_MAJOR_VERSION    0
+#define JPEGXL_MINOR_VERSION    6
+#define JPEGXL_PATCH_VERSION    0
+#define JPEGXL_LIBRARY_VERSION "0.6.0"
+
+#define JPEGXL_VERSION         "0.6.0.6b92549"
+		
