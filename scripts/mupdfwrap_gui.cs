aad76ca0c1d74ee2cbd4350ecbff4bf1f56a2558
Julian Smith <julian.smith@artifex.com>
2021-09-16 15:46:03 +0100

    scripts/mupdfwrap_gui.cs: wip

--- scripts/mupdfwrap_gui.cs
+++ scripts/mupdfwrap_gui.cs
@@ -1,16 +1,17 @@
-using System;
-using mupdf;
+//using System;
+//using System.Drawing;
+//using mupdf;
 
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Mono World");
+        System.Console.WriteLine("Hello Mono World");
         mupdf.Document document = new mupdf.Document("zlib.clean.pdf");
-        Console.WriteLine("num chapters: " + document.count_chapters());
+        System.Console.WriteLine("num chapters: " + document.count_chapters());
         mupdf.Page page = document.load_page(0);
         mupdf.Rect rect = page.bound_page();
-        Console.WriteLine("rect: " + rect.to_string());
+        System.Console.WriteLine("rect: " + rect.to_string());
 
         mupdf.Matrix matrix = new mupdf.Matrix(1, 0, 0, 1, 0, 0);
         mupdf.Colorspace colorspace = new mupdf.Colorspace(mupdf.Colorspace.Fixed.Fixed_RGB);
@@ -18,6 +19,18 @@ public class HelloWorld
                 matrix,
                 colorspace,
                 0
+                );
+        System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(
+                pixmap.pixmap_width(),
+                pixmap.pixmap_height(),
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb
+                );
+        //Bitmap^ bmp = gcnew System.Drawing.Bitmap(pixmap.pixmap_width(), pixmap.pixmap_height(), System.Drawing.Imaging.Format24bppRgb);
+        System.Drawing.Rectangle rect2 = new System.Drawing.Rectangle(0, 0, pixmap.pixmap_width(), pixmap.pixmap_height());
+        System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(
+                rect2,
+                System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp.PixelFormat
                 );
     }
 }
