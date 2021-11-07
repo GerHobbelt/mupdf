cbcea73ebb8ca8e4817ad2e9e45455ab74085ceb
Ger Hobbelt <ger@hobbelt.com>
2021-11-07 20:46:43 +0100

    fix MSVC warning in monolithic builds: warning MSB8027: Two or more files with the name of XXXXXXXX.cpp will produce outputs to the same location. This can lead to an incorrect build result.

---
+++ scripts/wxWidgets/jumbo-source01.cpp
@@ -0,0 +1,260 @@
+
+
+//------------------------------------------------------------------------
+// bmpbndl.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/common/bmpbndl.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/bmpbndl.cpp"
+
+//------------------------------------------------------------------------
+// secretstore.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/common/secretstore.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/secretstore.cpp"
+
+//------------------------------------------------------------------------
+// uilocale.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/common/uilocale.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/uilocale.cpp"
+
+//------------------------------------------------------------------------
+// accel.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/accel.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/accel.cpp"
+
+//------------------------------------------------------------------------
+// buttonbar.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/buttonbar.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/ribbon/buttonbar.cpp"
+
+//------------------------------------------------------------------------
+// caret.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/caret.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/caret.cpp"
+
+//------------------------------------------------------------------------
+// colour.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/colour.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/colour.cpp"
+
+//------------------------------------------------------------------------
+// desktopenv.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/desktopenv.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/desktopenv.cpp"
+
+//------------------------------------------------------------------------
+// fdrepdlg.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/fdrepdlg.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/fdrepdlg.cpp"
+
+//------------------------------------------------------------------------
+// icon.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/icon.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/icon.cpp"
+
+//------------------------------------------------------------------------
+// imaglist.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/imaglist.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/imaglist.cpp"
+
+//------------------------------------------------------------------------
+// listctrl.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/listctrl.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/listctrl.cpp"
+
+//------------------------------------------------------------------------
+// notebook.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/notebook.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/notebook.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/notebook.cpp"
+
+//------------------------------------------------------------------------
+// statline.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/statline.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/statline.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/statline.cpp"
+
+//------------------------------------------------------------------------
+// statusbr.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/statusbr.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/statusbr.cpp"
+
+//------------------------------------------------------------------------
+// textmeasure.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/textmeasure.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/textmeasure.cpp"
+
+//------------------------------------------------------------------------
+// timer.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/generic/timer.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/timer.cpp"
+
+//------------------------------------------------------------------------
+// anybutton.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/anybutton.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/anybutton.cpp"
+
+//------------------------------------------------------------------------
+// bmpbuttn.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/bmpbuttn.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/bmpbuttn.cpp"
+
+//------------------------------------------------------------------------
+// button.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/button.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/button.cpp"
+
+//------------------------------------------------------------------------
+// checkbox.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/checkbox.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/checkbox.cpp"
+
+//------------------------------------------------------------------------
+// checklst.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/checklst.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/checklst.cpp"
+
+//------------------------------------------------------------------------
+// choice.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/choice.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/choice.cpp"
+
+//------------------------------------------------------------------------
+// combobox.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/combobox.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/combobox.cpp"
+
+//------------------------------------------------------------------------
+// control.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/control.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/ribbon/control.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/control.cpp"
+
+//------------------------------------------------------------------------
+// dialog.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/dialog.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/dialog.cpp"
+
+//------------------------------------------------------------------------
+// gauge.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/gauge.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/gauge.cpp"
+
+//------------------------------------------------------------------------
+// listbox.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/listbox.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/listbox.cpp"
+
+//------------------------------------------------------------------------
+// menu.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/menu.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/menu.cpp"
+
+//------------------------------------------------------------------------
+// radiobox.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/radiobox.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/radiobox.cpp"
+
+//------------------------------------------------------------------------
+// radiobut.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/radiobut.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/radiobut.cpp"
+
+//------------------------------------------------------------------------
+// scrolbar.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/scrolbar.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/scrolbar.cpp"
+
+//------------------------------------------------------------------------
+// slider.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/slider.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/slider.cpp"
+
+//------------------------------------------------------------------------
+// spinbutt.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/spinbutt.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/spinbutt.cpp"
+
+//------------------------------------------------------------------------
+// stackwalk.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/stackwalk.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/unix/stackwalk.cpp"
+
+//------------------------------------------------------------------------
+// statbmp.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/statbmp.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/statbmp.cpp"
+
+//------------------------------------------------------------------------
+// statbox.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/statbox.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/statbox.cpp"
+
+//------------------------------------------------------------------------
+// stattext.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/stattext.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/stattext.cpp"
+
+//------------------------------------------------------------------------
+// stdpaths.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/stdpaths.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/unix/stdpaths.cpp"
+
+//------------------------------------------------------------------------
+// textctrl.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/textctrl.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/textctrl.cpp"
+
+//------------------------------------------------------------------------
+// tglbtn.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/tglbtn.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/tglbtn.cpp"
+
+//------------------------------------------------------------------------
+// toolbar.cpp
+
+#include "../../thirdparty/owemdjee/wxWidgets/src/msw/toolbar.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/ribbon/toolbar.cpp"
+#include "../../thirdparty/owemdjee/wxWidgets/src/univ/toolbar.cpp"
+
+
+
