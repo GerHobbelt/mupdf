1746e26d59b70dfe30bb37d0303ab303eb2a6055
Julian Smith <julian.smith@artifex.com>
2021-12-07 10:37:42 +0000

    source/: lots of diagnostics.

--- source/pdf/pdf-object.c
+++ source/pdf/pdf-object.c
@@ -2953,7 +2953,11 @@ static void fmt_dict(fz_context *ctx, struct fmt *fmt, pdf_obj *obj)
 static void fmt_obj(fz_context *ctx, struct fmt *fmt, pdf_obj *obj)
 {
 	char buf[256];
-
+	fprintf(stderr, "%s:%i:%s: obj=%p\n", __FILE__, __LINE__, __FUNCTION__, obj);
+	if ((intptr_t) obj > 4000)
+	{
+		fprintf(stderr, "%s:%i:%s: obj->kind=%i\n", __FILE__, __LINE__, __FUNCTION__, obj->kind);
+	}
 	if (obj == PDF_NULL)
 		fmt_puts(ctx, fmt, "null");
 	else if (obj == PDF_TRUE)
@@ -2995,7 +2999,12 @@ static void fmt_obj(fz_context *ctx, struct fmt *fmt, pdf_obj *obj)
 	else if (pdf_is_dict(ctx, obj))
 		fmt_dict(ctx, fmt, obj);
 	else
-		fmt_puts(ctx, fmt, "<unknown object>");
+	{
+		char buffer[64];
+		snprintf(buffer, sizeof(buffer), "<unknown object %i>", obj->kind);
+		fmt_puts(ctx, fmt, buffer);
+		//fmt_puts(ctx, fmt, "<unknown object>");
+	}
 }
 
 static char *
