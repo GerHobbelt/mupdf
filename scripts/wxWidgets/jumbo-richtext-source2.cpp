60154580d6fa65d7c081cafe316159e7482ff510
Ger Hobbelt <ger@hobbelt.com>
2021-12-12 04:56:56 +0100

    fix MSVC2019 compiler warnings about the need for `/bigobj` (which, as a compiler option, seems to slow down compile/build significantly) by splitting the indicated jumbo files in half.

--- scripts/wxWidgets/jumbo-richtext-source2.cpp
+++ scripts/wxWidgets/jumbo-richtext-source2.cpp
@@ -5,66 +5,6 @@
 
 
 //------------------------------------------------------------------------
-// richtextbackgroundpage.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextbackgroundpage.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextborderspage.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextborderspage.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextbuffer.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextbuffer.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextbulletspage.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextbulletspage.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextctrl.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextctrl.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextfontpage.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextfontpage.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextformatdlg.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextformatdlg.cpp"
-
-
-//------------------------------------------------------------------------
-// richtexthtml.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtexthtml.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextimagedlg.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextimagedlg.cpp"
-
-
-//------------------------------------------------------------------------
-// richtextindentspage.cpp
- 
-#include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextindentspage.cpp"
-
-
-//------------------------------------------------------------------------
 // richtextliststylepage.cpp
  
 #include "../../thirdparty/owemdjee/wxWidgets/src/richtext/richtextliststylepage.cpp"
