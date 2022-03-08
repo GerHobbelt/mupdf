96791df366d8199609f8dcb149a1651d8500bb6f
Ger Hobbelt <ger@hobbelt.com>
2022-03-08 20:37:33 +0100

    update wxWidgets MSVC project file

--- platform/win32/refill-vcxproj.js
+++ platform/win32/refill-vcxproj.js
@@ -103,6 +103,13 @@ if (projname.endsWith("_examples")) {
 	checkDirAndReportPlusExitOnSuccess(testpath);
 }
 
+if (projname.endsWith("_DLL")) {
+	let tst_projname = projname.replace(/_DLL$/, '');
+	testpath = `${scriptpath}/../../thirdparty/owemdjee/${tst_projname}`;
+
+	checkDirAndReportPlusExitOnSuccess(testpath);
+}
+
 // now things get a little hairier: we need to MAP to projectname to a submodule directory path...
 
 const projectMap = {
