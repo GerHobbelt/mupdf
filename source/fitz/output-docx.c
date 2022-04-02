4c74cd816508491906d34dac722b432d18e9153b
Julian Smith <julian.smith@artifex.com>
2022-04-02 11:28:29 +0100

    source/fitz/output-docx.c: fixes for latest thirdparty/extract.

--- source/fitz/output-docx.c
+++ source/fitz/output-docx.c
@@ -108,7 +108,14 @@ static void dev_text(fz_context *ctx, fz_device *dev_, const fz_text *text, fz_m
 				if (span->items[i].gid >= 0)
 					adv = fz_advance_glyph(ctx, span->font, span->items[i].gid, span->wmode);
 
-				if (extract_add_char(dev->writer->extract, item->x, item->y, item->ucs, adv, 0 /*autosplit*/))
+				if (extract_add_char(
+						dev->writer->extract,
+						item->x,
+						item->y,
+						item->ucs,
+						adv, 0 /*autosplit*/,
+						0, 0, 0, 0 /* bbox. */
+						))
 					fz_throw(ctx, FZ_ERROR_GENERIC, "Failed to add char");
 			}
 
@@ -378,7 +385,7 @@ static fz_device *writer_begin_page(fz_context *ctx, fz_document_writer *writer_
 	fz_var(dev);
 	fz_try(ctx)
 	{
-		if (extract_page_begin(writer->extract))
+		if (extract_page_begin(writer->extract, 0, 0, 0, 0 /* mediabox */))
 			fz_throw(ctx, FZ_ERROR_GENERIC, "Failed to begin page");
 		dev = fz_new_derived_device(ctx, fz_docx_device);
 		dev->super.fill_text = dev_fill_text;
