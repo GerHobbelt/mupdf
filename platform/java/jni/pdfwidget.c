fea2c9d44e6e01e219f9b9c0df0393760ebb8fce
Sebastian Rasmussen <sebras@gmail.com>
2022-05-04 14:07:47 +0200

    jni: Setting value should count as an undoable operation.

--- platform/java/jni/pdfwidget.c
+++ platform/java/jni/pdfwidget.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2004-2021 Artifex Software, Inc.
+// Copyright (C) 2004-2022 Artifex Software, Inc.
 //
 // This file is part of MuPDF.
 //
@@ -104,7 +104,7 @@ FUN(PDFWidget_setValue)(JNIEnv *env, jobject self, jstring jval)
 
 	fz_var(accepted);
 	fz_try(ctx)
-		accepted = pdf_set_field_value(ctx, pdf_annot_page(ctx, widget)->doc, pdf_annot_obj(ctx, widget), (char *)val, pdf_get_widget_editing_state(ctx, widget));
+		accepted = pdf_set_annot_field_value(ctx, pdf_annot_page(ctx, widget)->doc, pdf_annot_obj(ctx, widget), (char *)val, pdf_get_widget_editing_state(ctx, widget));
 	fz_always(ctx)
 		if (jval)
 			(*env)->ReleaseStringUTFChars(env, jval, val);
