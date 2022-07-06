eed5967acc49747ecfa9c805e4e3bd62d1c46d2c
Ger Hobbelt <ger@hobbelt.com>
2022-07-06 20:58:54 +0200

    refactoring: using the new monolithic build template code for the dispatchers in every monolith.

--- scripts/libclipp/monolithic_main.c
+++ scripts/libclipp/monolithic_main.c
@@ -1,34 +1,14 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#ifdef _MSC_VER
-#include <io.h>
-#else
-#include <unistd.h>
-#endif
-#include <errno.h>
-
 #define BUILD_MONOLITHIC 1
 #include "examples/monolithic_examples.h"
 
+#include "monolithic_examples.h"
 
-typedef int tool_f();
-typedef int tool_fa(int argc, const char** argv);
+#define USAGE_NAME   "clipp"
 
-static int usage();
+#include "monolithic_main_internal_defs.h"
 
-
-static struct cmd_info
-{
-	const char* cmd;
-	union
-	{
-		tool_f* f;
-		tool_fa* fa;
-	} f;
-} commands[] = {
+MONOLITHIC_CMD_TABLE_START()
 	{ "clipp_actions", {.fa = clipp_actions_main } },
 	{ "clipp_align", {.fa = clipp_align_main } },
 	{ "clipp_alternatives", {.fa = clipp_alternatives_main } },
@@ -63,65 +43,6 @@ static struct cmd_info
 	{ "clipp_transform", {.fa = clipp_transform_main } },
 
 	{ "clipp_sanity", {.f = clipp_sanity_main } },
+MONOLITHIC_CMD_TABLE_END();
 
-	{ "?", { .f = usage } },
-	{ "h", {.f = usage } },
-	{ "help", {.f = usage } },
-	{ "-?", {.f = usage } },
-	{ "-h", {.f = usage } },
-	{ "--help", {.f = usage } },
-};
-
-
-static void help(void)
-{
-	fprintf(stderr, "Commands:\n");
-	for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
-	{
-		struct cmd_info el = commands[i];
-		fprintf(stderr, "  %s\n", el.cmd);
-	}
-}
-
-
-static int usage()
-{
-	fprintf(stderr, "Usage: clipp_demo [command]\n");
-	help();
-	return 1;
-}
-
-
-static int parse(const char* source, int argc, const char** argv)
-{
-	if (source)
-	{
-		for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
-		{
-			struct cmd_info el = commands[i];
-			size_t cmd_len = strlen(el.cmd);
-			char sentinel = 0;
-
-			if (strlen(source) >= cmd_len)
-				sentinel = source[cmd_len];
-
-			if (strncmp(source, el.cmd, cmd_len) == 0 && (sentinel == 0 || isspace(sentinel)))
-			{
-				return el.f.fa(argc, argv);
-			}
-		}
-
-		fprintf(stderr, "Unknown command '%s'.\n\nUse 'h' or 'help' command to get a list of supported commands.\n", source);
-		return 1;
-	}
-	else
-	{
-		return usage();
-	}
-}
-
-int main(int argc, const char** argv)
-{
-	return parse(argv[1], argc - 1, argv + 1);
-}
-
+#include "monolithic_main_tpl.h"
--- scripts/libxml2/monolithic_main.c
+++ scripts/libxml2/monolithic_main.c
@@ -1,36 +1,16 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#ifdef _MSC_VER
-#include <io.h>
-#else
-#include <unistd.h>
-#endif
-#include <errno.h>
-
 #include "zlib-ng.h"
 
 #define BUILD_MONOLITHIC 1
 #include "libxml/monolithic_examples.h"
 
+#include "monolithic_examples.h"
 
-typedef int tool_f();
-typedef int tool_fa(int argc, const char** argv);
+#define USAGE_NAME   "xml"
 
-static int usage();
+#include "monolithic_main_internal_defs.h"
 
-
-static struct cmd_info
-{
-	const char* cmd;
-	union
-	{
-		tool_f* f;
-		tool_fa* fa;
-	} f;
-} commands[] = {
+MONOLITHIC_CMD_TABLE_START()
 	{ "xml_api", {.fa = xml_testapi_main } },
 	{ "xml_Automata", {.fa = xml_testAutomata_main } },
 	{ "xml_C14N", {.fa = xml_testC14N_main } },
@@ -63,67 +43,6 @@ static struct cmd_info
 
 	{ "xml_threads", {.f = xml_testthreads_main } },
 	{ "xml_trionan", {.f = xml_trionan_main } },
+MONOLITHIC_CMD_TABLE_END();
 
-	{ "?", {.f = usage } },
-	{ "h", {.f = usage } },
-	{ "help", {.f = usage } },
-	{ "-?", {.f = usage } },
-	{ "-h", {.f = usage } },
-	{ "--help", {.f = usage } },
-};
-
-
-static void help(void)
-{
-	fprintf(stderr, "Commands:\n");
-	for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
-	{
-		struct cmd_info el = commands[i];
-		fprintf(stderr, "  %s\n", el.cmd);
-	}
-}
-
-
-static int usage()
-{
-	fprintf(stderr, "Usage: clipp_demo [command]\n");
-	help();
-	return 1;
-}
-
-
-static int parse(const char* source, int argc, const char** argv)
-{
-	if (source)
-	{
-		for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
-		{
-			struct cmd_info el = commands[i];
-			size_t cmd_len = strlen(el.cmd);
-			char sentinel = 0;
-
-			if (strlen(source) >= cmd_len)
-				sentinel = source[cmd_len];
-
-			if (strncmp(source, el.cmd, cmd_len) == 0 && (sentinel == 0 || isspace(sentinel)))
-			{
-				return el.f.fa(argc, argv);
-			}
-		}
-
-		fprintf(stderr, "Unknown command '%s'.\n\nUse 'h' or 'help' command to get a list of supported commands.\n", source);
-		return 1;
-	}
-	else
-	{
-		return usage();
-	}
-}
-
-int main(int argc, const char** argv)
-{
-	zng_lib_init();
-
-	return parse(argv[1], argc - 1, argv + 1);
-}
-
+#include "monolithic_main_tpl.h"
