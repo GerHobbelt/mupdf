bf90c38d6f15dcaf699ee043957049ff2871c9d4
Ger Hobbelt <ger@hobbelt.com>
2022-09-02 22:46:41 +0200

    - add new `fz_set_cpu_core_count(multiplier, lower_bound, upper_bound)` API so tools can tweak their desired number of threads, etc. - `muraster` now sniffs the number of cores on your machine and uses that as the default, akin to `mudraw -P` behaviour.

--- include/mupdf/helpers/cpu.h
+++ include/mupdf/helpers/cpu.h
@@ -13,8 +13,26 @@ extern "C" {
   Return the number of logical cores in the CPU.
 
   Return 0 if this number cannot be determined.
+
+  NOTE: this datapoint is determines once, at the time of the first call into this function.
+  When you wish to adjust or re-set the core count returned by this function later on, you can use the `fz_set_cpu_core_count()` API,
+  where you can specify a multiplying factor and lower+upper bounds on this number.
 */
 int fz_get_cpu_core_count(void);
+
+/**
+  Set the number of logical cores in the CPU, multiplied by a factor (`perunage_or_real_number_of_cores`): 1.0f means 100%, i.e. take the real value.
+
+  A lower and upper bound can be specified:
+  - when the lower bound is 2 or less, 2 is assumed, i.e. the lower bound in the number of cores will always be 2 or greater.
+  - when the upper bound is below the lower bound, it is assumed to equal the lower bound.
+
+    Hence to quickly set a hardcoded, fixed, number of cores is to set the number in the lower and upper bound parameters,
+    irrespective of the multiplier.
+
+  Returns the number of cores set.
+*/
+int fz_set_cpu_core_count(float perunage_or_real_number_of_cores, int min_count, int max_count);
 
 #ifdef __cplusplus
 }
--- source/helpers/cpu_core_count.cpp
+++ source/helpers/cpu_core_count.cpp
@@ -4,6 +4,7 @@
 #include "mupdf/helpers/cpu.h"
 
 #include <thread>
+#include <math.h>
 
 static int processor_count = 0;
 
@@ -14,5 +15,30 @@ extern "C" int fz_get_cpu_core_count(void)
 		// may return 0 when not able to detect
 		processor_count = std::thread::hardware_concurrency();
 	}
+	return processor_count;
+}
+
+extern "C" int fz_set_cpu_core_count(float perunage_or_real_number_of_cores, int min_count, int max_count)
+{
+	// may return 0 when not able to detect
+	int count = max(2, std::thread::hardware_concurrency());
+
+	if (min_count < 2)
+		min_count = 2;
+
+	if (max_count < count)
+		max_count = count;
+	else if (max_count < min_count)
+		max_count = min_count;
+
+	int i = lroundf(count * perunage_or_real_number_of_cores);
+
+	if (i > max_count)
+		i = max_count;
+	else if (i < min_count)
+		i = min_count;
+
+	processor_count = i;
+
 	return processor_count;
 }
