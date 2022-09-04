e7d53d0057a153c593a28bae90f1eb7167769aa6
Ger Hobbelt <ger@hobbelt.com>
2022-09-04 21:34:25 +0200

    cpu count: added `fz_parse_pool_threadcount_preference()` API to help commandline tools and configuration file readers get the user preference re threadcount to use.
    ---
    
    Parse the requested thread count (string value).
    
    You can specify the thread count in various ways:
    
    - as a direct POSITIVE integer number, e.g. "4"
    - as equal to the number of (virtual) cores available on your machine: "N" or "*"
    - as a *percentage* of the number of (virtual) cores available on your machine, e.g. "50%" or "200%" (the latter will then, obviously, allocate twice as many threads as you have cores)
    - as a NEGATIVE integer number, meaning "number of cores MINUS this number", e.g. "-1". This comes in handy when you want to keep one or more cores completely free and available for other work, e.g. GUI message pumping.
    
    When the input string value is NULL or does not match any of the above, the returned value is "number of available cores", i.e. identical to when you would have specified `"N"`.
    
    All parsed input values are sanity-checked and are clipped to a "sensible range": the accepted input is \[1 .. N*32\] (i.e.: "from 1 up to and including 32 times the number of available CPU cores")
    
    When the number of available CPU cores cannot be properly detected by the underlying code, *4* is assumed as most medium-grade consumer hardware these days is based on 2 cores times 2 ("hyperthreading") hence *4* virtual cores.
    
    You MAY override the default count ("N") -- used in case the parse failed to match any of the rules stated above -- by specifying an explicit default via a non-NULL `default_count` argument. This value is NOT sanity-checked and range-limited following the rules above, so you MAY pass in, f.e., a NEGATIVE number to signal a parse error, to be further dealt with by your userland code.
    
    P.S.: when you wish to provide a different number for `N`, we advise you to use the `fz_set_cpu_core_count()` API before this call.

--- source/helpers/cpu_core_count.cpp
+++ source/helpers/cpu_core_count.cpp
@@ -12,8 +12,10 @@ extern "C" int fz_get_cpu_core_count(void)
 {
 	if (!processor_count)
 	{
-		// may return 0 when not able to detect
-		processor_count = std::thread::hardware_concurrency();
+		// std::thread::hardware_concurrency() may return 0 when not able to detect.
+		//
+		// use fz_set_cpu_core_count() instead.
+		return fz_set_cpu_core_count(0.0f, 0, INT_MAX);
 	}
 	return processor_count;
 }
@@ -21,15 +23,21 @@ extern "C" int fz_get_cpu_core_count(void)
 extern "C" int fz_set_cpu_core_count(float perunage_or_real_number_of_cores, int min_count, int max_count)
 {
 	// may return 0 when not able to detect
-	int count = max(2, std::thread::hardware_concurrency());
+	//
+	// When this happens, *2* is assumed as we expect these failures to occur on older hardware, which usually has only 1 or 2 cores.
+	int count = std::thread::hardware_concurrency();
+
+	if (perunage_or_real_number_of_cores == 0.0f)  // no need for approx match
+		perunage_or_real_number_of_cores = 1.0f;
 
 	if (min_count < 2)
 		min_count = 2;
 
+	if (count < min_count)
+		count = min_count;
+
 	if (max_count < count)
 		max_count = count;
-	else if (max_count < min_count)
-		max_count = min_count;
 
 	int i = lroundf(count * perunage_or_real_number_of_cores);
 
@@ -41,4 +49,90 @@ extern "C" int fz_set_cpu_core_count(float perunage_or_real_number_of_cores, int
 	processor_count = i;
 
 	return processor_count;
+}
+
+extern "C" int fz_parse_pool_threadcount_preference(const char *spec, int min_count, int max_count, int default_count)
+{
+	int N = fz_get_cpu_core_count();
+
+	int dflt = N;
+	if (default_count)
+		dflt = default_count;
+
+	if (!min_count)
+		min_count = 1;
+
+	if (!max_count)
+		max_count = N * 32;
+
+	if (max_count < min_count)
+		max_count = min_count;
+
+	int count = N;
+
+	int v = dflt;
+
+	if (spec)
+	{
+		// skip whitespace
+		spec += strspn(spec, " \t\r\n");
+
+		if (toupper(*spec) == 'N' || *spec == '*')
+		{
+			v = N;
+
+			// clip input to sanity range:
+			if (v < min_count)
+				v = min_count;
+			else if (v > max_count)
+				v = max_count;
+
+			spec++;
+		}
+		else
+		{
+			const char *p = spec;
+			long lv = strtol(spec, (char **)&p, 0);
+
+			// skip whitespace
+			p += strspn(p, " \t\r\n");
+
+			// when there's still an input parse tail, the parse via strtol() is considered FAILED,
+			// UNLESS it's a '%' character and nothing else.
+			//
+			// when it did not consume any characters, we look for 'N' & '*'; anything else a FAIL.
+			if (p > spec && *p == '%')
+			{
+				p++;
+
+				lv = (N * lv) / 100;
+			}
+			else if (lv < 0)
+			{
+				lv = N - lv;
+			}
+
+			// clip input to sanity range:
+			if (lv < min_count)
+				v = min_count;
+			else if (lv > max_count)
+				v = max_count;
+			else
+				v = (int)lv;
+
+			spec = p;
+		}
+
+		// skip whitespace
+		spec += strspn(spec, " \t\r\n");
+
+
+		// any tail? yes --> FAIL.
+		if (*spec)
+		{
+			v = dflt;
+		}
+	}
+
+	return v;
 }
