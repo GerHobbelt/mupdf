aee80b9c36c4a1c5ffa2024f2b5bfaedb52bb23d
Ger Hobbelt <ger@hobbelt.com>
2022-09-26 01:14:01 +0200

    updated submodules; also split wxWidgets benchmarks' code into multiple projects to fix unresolvable collisions (due to monolithic mode build)

--- scripts/libjpeg-xl/tool_version_git.h
+++ scripts/libjpeg-xl/tool_version_git.h
@@ -2,9 +2,9 @@
 #pragma once
 
 #define JPEGXL_MAJOR_VERSION    0
-#define JPEGXL_MINOR_VERSION    6
+#define JPEGXL_MINOR_VERSION    7
 #define JPEGXL_PATCH_VERSION    0
-#define JPEGXL_LIBRARY_VERSION "0.6.0"
+#define JPEGXL_LIBRARY_VERSION "0.7.0"
 
-#define JPEGXL_VERSION         "0.6.0.339d53e9"
+#define JPEGXL_VERSION         "0.7.0.5e7acd92"
 		
