0a15f5a1fe859339d2353c57cb536a10144f8068
Ger Hobbelt <ger@hobbelt.com>
2023-01-21 16:35:52 +0100

    monolithic build wrapper app template: add check + #error to its monolithic_examples.h example header file so any projects which accidentally load that one instead of their own get a hard failure at compile time. (This was useful while working on cmph_hasher, et al, et least.)

--- thirdparty/monolithic_tmplt/monolithic_examples.h
+++ thirdparty/monolithic_tmplt/monolithic_examples.h
@@ -4,6 +4,10 @@
 
 #pragma once
 
+#if !defined(MONOLITHIC_TEMPLATE_DEMO)
+#error "You are missing an include path pointing at your own monolithic_example.h as you are loading this template demo one instead!"
+#endif
+
 #if defined(BUILD_MONOLITHIC)
 
 #ifdef __cplusplus
