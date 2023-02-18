6a2da19040eac0311f05799b60aeee219995adf1
Ger Hobbelt <ger@hobbelt.com>
2023-02-18 13:18:01 +0100

    (yech.) Got fed up with the boost conglomerate but many submodules use it. Adding the boost hierarchy as symlink-like files so I only have to add one include path (../../scripts/include) when a project happens to require any of this stuff...

---
+++ scripts/boost/include/boost/phoenix/operator/detail/cpp03/mem_fun_ptr_eval_result_of.hpp
@@ -0,0 +1,10 @@
+//
+// symlink-like file is generated using the platform/win32/prep_boost_include_directories.sh shell script.
+//
+// ** DO NOT EDIT ** (adjust the regenerator shell script mentioned above instead!)
+//
+
+#pragma once
+
+#include "../../../thirdparty/owemdjee/boost/libs/phoenix/include/boost/phoenix/operator/detail/cpp03/mem_fun_ptr_eval_result_of.hpp"
+
