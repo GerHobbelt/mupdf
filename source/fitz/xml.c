0fb044c30fadc1a0ccf8ceed958e134eb468b8a3
Ger Hobbelt <ger@hobbelt.com>
2023-07-04 00:40:02 +0200

    fix bu: reported as **compiler warning** by MSVC2022 but is a fatal bug.

--- source/fitz/xml.c
+++ source/fitz/xml.c
@@ -821,7 +821,7 @@ parse_closing_element:
 	p = jump_past_white(p);
 	mark = p;
 	p = jump_past_name(p);
-	if (!isname(*mark))
+	if (!isname(mark))
 		return "syntax error in closing element";
 	if (close_tag(ctx, parser, mark, p))
 		return "opening and closing tag mismatch";
