07b85e0101bf7e63bf0cf70103a87da18bd7d5c9
Sebastian Rasmussen <sebras@gmail.com>
2023-07-26 23:57:24 +0200

    RFC: Revert "Similar updated to bug 706942..."
    This reverts commit 66e39eb61287638e103e0bb079aa086cff9a38d7.

--- source/pdf/pdf-annot.c
+++ source/pdf/pdf-annot.c
@@ -1573,7 +1573,7 @@ pdf_annot_border_dash_count(fz_context *ctx, pdf_annot *annot)
 		d = pdf_dict_get(ctx, bs, PDF_NAME(D));
 		/* Query legacy dash pattern as a fallback */
 		border = pdf_dict_get(ctx, annot->obj, PDF_NAME(Border));
-		if (pdf_is_null(ctx, d) && pdf_is_array(ctx, border))
+		if (!d && pdf_is_array(ctx, border))
 			d = pdf_array_get(ctx, border, 3);
 		count = pdf_array_len(ctx, d);
 	}
@@ -1600,7 +1600,7 @@ pdf_annot_border_dash_item(fz_context *ctx, pdf_annot *annot, int i)
 		d = pdf_dict_get(ctx, bs, PDF_NAME(D));
 		/* Query legacy dash pattern as a fallback */
 		border = pdf_dict_get(ctx, annot->obj, PDF_NAME(Border));
-		if (pdf_is_null(ctx, d) && pdf_is_array(ctx, border))
+		if (!d && pdf_is_array(ctx, border))
 			d = pdf_array_get(ctx, border, 3);
 		length = pdf_array_get_real(ctx, d, i);
 	}
@@ -3314,7 +3314,7 @@ pdf_set_annot_appearance(fz_context *ctx, pdf_annot *annot, const char *appearan
 			pdf_drop_obj(ctx, form);
 			form = NULL;
 		}
-		if (!pdf_is_dict(ctx, form))
+		if (!form)
 			form = pdf_new_xobject(ctx, annot->page->doc, bbox, ctm, res, contents);
 		else
 			pdf_update_xobject(ctx, annot->page->doc, form, bbox, ctm, res, contents);
