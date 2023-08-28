c480d41146ec99267a7c6484f64aa023b1fbdba4
Ger Hobbelt <ger@hobbelt.com>
2023-08-28 23:08:58 +0200

    DEBUG: turn on heap checking and heap leak reporting at app termination (MSVC2022)

--- source/helpers/heap_debugging.cpp
+++ source/helpers/heap_debugging.cpp
@@ -225,9 +225,9 @@ static int EnableMemLeakChecking(void)
 
 	//_CrtSetBreakAlloc(744);  /* Break at memalloc{744}, or 'watch' _crtBreakAlloc */
 	 
-	//const int desired_flags = (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_DELAY_FREE_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	const int desired_flags = (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_DELAY_FREE_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	//const int desired_flags = (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	const int desired_flags = (_CRTDBG_ALLOC_MEM_DF);
+	//const int desired_flags = (_CRTDBG_ALLOC_MEM_DF);
 	int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
 	flags |= desired_flags;
 	_CrtSetDbgFlag(flags);
