68f23acf2f894e63f024fd998b239b66d7139f0c
Ger Hobbelt <ger@hobbelt.com>
2023-11-25 02:10:06 +0100

    it's time: introducing OpenCV and boost to the monolithic build. (I don't like boost, but lots of peeps use it, so it sneaks in anyway...)

--- include/system-override/assert.h
+++ include/system-override/assert.h
@@ -135,7 +135,7 @@ namespace impl
 {
 	static inline /* explicit */ bool validateBool4Assert(bool b) { return b; }
 	static inline bool validateBool4Assert(const void* b) { return !!b; }
-	static inline bool validateBool4Assert(int) = delete; //catch unintended bool conversions, e.g. HRESULT
+	static inline bool validateBool4Assert(int) = delete;      // catch unintended bool conversions, e.g. HRESULT
 	static inline bool validateBool4Assert(float) = delete; 
 	static inline bool validateBool4Assert(double) = delete; 
 	static inline bool validateBool4Assert(unsigned long int) = delete; 
