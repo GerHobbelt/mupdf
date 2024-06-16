c877f4472236b0a0b25939c0b75378f24657060e
Ger Hobbelt <ger@hobbelt.com>
2024-06-16 22:08:12 +0200

    fixed a few compiler errors + code review: experimental tesseract/OCR interface/page-renderer: synced the code with the PCLm renderer as best I could. Also compared the experimental code against a few mupdf output renderer implementations to better ensure the code will deliver as expected.

--- source/fitz/output-pdfocr.c
+++ source/fitz/output-pdfocr.c
@@ -304,24 +304,24 @@ post_skew_write_header(fz_context *ctx, pdfocr_band_writer *writer, int w, int h
 	if (sh == 0)
 		sh = h;
 	assert(sh != 0 && "pdfocr_write_header() should not be given zero height input.");
-	strips = (h + sh-1)/sh;
+	strips = (h + sh - 1) / sh;
 
 	writer->deskewed_w = w;
 	writer->deskewed_h = h;
 
 	writer->stripbuf = Memento_label(fz_malloc(ctx, (size_t)w * sh * n), "pdfocr_stripbuf");
-	writer->complen = fz_deflate_bound(ctx, (size_t)w * sh * n);
+	writer->complen = zng_compressBound((size_t)w * sh * n);
 	writer->compbuf = Memento_label(fz_malloc(ctx, writer->complen), "pdfocr_compbuf");
 
 	/* Always round the width of ocrbitmap up to a multiple of 4. */
-	writer->ocrbitmap = fz_new_pixmap(ctx, NULL, (w+3)&~3, h, NULL, 0);
+	writer->ocrbitmap = fz_new_pixmap(ctx, NULL, (w + 3) & ~3, h, NULL, 0);
 	fz_set_pixmap_resolution(ctx, writer->ocrbitmap, xres, yres);
 
 	/* Send the Page Object */
-	fz_write_printf(ctx, out, "%d 0 obj\n<</Type/Page/Parent 2 0 R/Resources<</XObject<<", new_obj(ctx, writer));
+	fz_write_printf(ctx, out, "%d 0 obj\n<<\n/Type /Page\n/Parent 2 0 R\n/Resources <<\n/XObject <<\n", new_obj(ctx, writer));
 	for (i = 0; i < strips; i++)
-		fz_write_printf(ctx, out, "/I%d %d 0 R", i, writer->obj_num + i);
-	fz_write_printf(ctx, out, ">>/Font<</F0 3 0 R>>>>/MediaBox[0 0 %g %g]/Contents %d 0 R>>\nendobj\n",
+		fz_write_printf(ctx, out, "/Image%d %d 0 R\n", i, writer->obj_num + 1 + i);
+	fz_write_printf(ctx, out, ">>\n/Font<</F0 3 0 R>>\n>>\n/MediaBox[ 0 0 %g %g ]\n/Contents [ %d 0 R ]\n>>\nendobj\n",
 		w * 72.0f / xres, h * 72.0f / yres, writer->obj_num + strips);
 }
 
@@ -341,7 +341,7 @@ pdfocr_write_header(fz_context *ctx, fz_band_writer *writer_, fz_colorspace *cs)
 	if (sh == 0)
 		sh = h;
 	assert(sh != 0 && "pdfocr_write_header() should not be given zero height input.");
-	strips = (h + sh-1)/sh;
+	strips = (h + sh - 1) / sh;
 
 	if (a != 0)
 		fz_throw(ctx, FZ_ERROR_ARGUMENT, "PDFOCR cannot write alpha channel");
@@ -407,18 +407,21 @@ flush_strip(fz_context *ctx, pdfocr_band_writer *writer, int fill)
 	int w = writer->deskewed_w;
 	int n = writer->super.n;
 	size_t len = (size_t)w*n*fill;
+	int result;
 
 	/* Buffer is full, compress it and write it. */
 	if (writer->options.compress)
 	{
 		size_t destLen = writer->complen;
-		zng_compress2(writer->compbuf, &destLen, data, len, Z_BEST_COMPRESSION);
+		result = zng_compress2(writer->compbuf, &destLen, data, len, Z_BEST_COMPRESSION);
+		if (result != Z_OK)
+			fz_throw(ctx, FZ_ERROR_GENERIC, "zlib error when compressing strip");
 		len = destLen;
 		data = writer->compbuf;
 	}
-	fz_write_printf(ctx, out, "%d 0 obj\n<</Width %d/ColorSpace/Device%s/Height %d%s/Subtype/Image",
-		new_obj(ctx, writer), w, n == 1 ? "Gray" : "RGB", fill, writer->options.compress ? "/Filter/FlateDecode" : "");
-	fz_write_printf(ctx, out, "/Length %zd/Type/XObject/BitsPerComponent 8>>\nstream\n", len);
+	fz_write_printf(ctx, out, "%d 0 obj\n<<\n/Width %d\n/ColorSpace /Device%s\n/Height %d\n%s/Subtype /Image\n",
+		new_obj(ctx, writer), w, n == 1 ? "Gray" : "RGB", fill, writer->options.compress ? "/Filter /FlateDecode\n" : "");
+	fz_write_printf(ctx, out, "/Length %zd\n/Type /XObject\n/BitsPerComponent 8\n>>\nstream\n", len);
 	fz_write_data(ctx, out, data, len);
 	fz_write_string(ctx, out, "\nendstream\nendobj\n");
 }
@@ -435,6 +438,9 @@ post_skew_write_band(fz_context *ctx, pdfocr_band_writer *writer, int stride, in
 	int line;
 	unsigned char *d;
 
+	if (!out)
+		return;
+
 	if (sh == 0)
 		sh = h;
 
@@ -442,11 +448,12 @@ post_skew_write_band(fz_context *ctx, pdfocr_band_writer *writer, int stride, in
 	{
 		int dstline = (band_start+line) % sh;
 		memcpy(writer->stripbuf + (size_t)w*n*dstline,
-			sp + (size_t)line * w * n,
-			(size_t)w * n);
+			   sp + (size_t)line * w * n,
+			   (size_t)w * n);
 		if (dstline+1 == sh)
 			flush_strip(ctx, writer, dstline+1);
 	}
+
 	if (band_start + band_height == h && h % sh != 0)
 		flush_strip(ctx, writer, h % sh);
 
@@ -910,7 +917,7 @@ pdfocr_write_trailer(fz_context *ctx, fz_band_writer *writer_)
 				this_sh += at;
 				at = 0;
 			}
-			fz_append_printf(ctx, buf, "/P <</MCID 0>> BDC\nq\n%d 0 0 %d 0 %d cm\n/I%d Do\nQ\n",
+			fz_append_printf(ctx, buf, "/P <</MCID 0>> BDC\nq\n%d 0 0 %d 0 %d cm\n/Image%d Do\nQ\n",
 				w, this_sh, at, i);
 		}
 
@@ -953,26 +960,22 @@ pdfocr_close_band_writer(fz_context *ctx, fz_band_writer *writer_)
 
 		/* Catalog */
 		writer->xref[1] = fz_tell_output(ctx, out);
-		fz_write_printf(ctx, out, "1 0 obj\n<</Type/Catalog/Pages 2 0 R>>\nendobj\n");
+		fz_write_printf(ctx, out, "1 0 obj\n<<\n/Type /Catalog\n/Pages 2 0 R\n>>\nendobj\n");
 
 		/* Page table */
 		writer->xref[2] = fz_tell_output(ctx, out);
-		fz_write_printf(ctx, out, "2 0 obj\n<</Count %d/Kids[", writer->pages);
+		fz_write_printf(ctx, out, "2 0 obj\n<<\n/Count %d\n/Kids [ ", writer->pages);
 
 		for (i = 0; i < writer->pages; i++)
-		{
-			if (i > 0)
-				fz_write_byte(ctx, out, ' ');
-			fz_write_printf(ctx, out, "%d 0 R", writer->page_obj[i]);
-		}
-		fz_write_string(ctx, out, "]/Type/Pages>>\nendobj\n");
+			fz_write_printf(ctx, out, "%d 0 R ", writer->page_obj[i]);
+		fz_write_string(ctx, out, "]\n/Type /Pages\n>>\nendobj\n");
 
 		/* Xref */
 		t_pos = fz_tell_output(ctx, out);
 		fz_write_printf(ctx, out, "xref\n0 %d\n0000000000 65535 f \n", writer->obj_num);
 		for (i = 1; i < writer->obj_num; i++)
-			fz_write_printf(ctx, out, "%010ld 00000 n \n", writer->xref[i]);
-		fz_write_printf(ctx, out, "trailer\n<</Size %d/Root 1 0 R>>\nstartxref\n%ld\n%%%%EOF\n", writer->obj_num, t_pos);
+			fz_write_printf(ctx, out, "%010zd 00000 n \n", writer->xref[i]);
+		fz_write_printf(ctx, out, "trailer\n<<\n/Size %d\n/Root 1 0 R\n>>\nstartxref\n%ld\n%%%%EOF\n", writer->obj_num, t_pos);
 	}
 }
 
