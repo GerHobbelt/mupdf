b2957f436acc25462a042ad64f0d5f058d948868
Robin Watts <Robin.Watts@artifex.com>
2024-07-22 19:11:50 +0100

    Simple mutool stamp tool.
    Add a "stamp" (akin to watermark) onto the bottom left of every
    page of a PDF.
    
    # Conflicts:
    #	platform/win32/libmutool.vcxproj
    #	source/tools/mutool.c

--- source/tools/mutool.c
+++ source/tools/mutool.c
@@ -130,6 +130,7 @@ static struct tool_spec {
 	{ {.fa = pdfbake_main }, "bake", "bake PDF form into static content" },
 	{ {.fa = pdftagged_main }, "tagged", "extract Tagged PDF content" },
 	{ {.fa = pdftag_main }, "tag", "perform operations on PDF tags" },
+	{ {.fa = pdfstamp_main }, "stamp", "Add a stamp onto PDF pages" },
 	{ {.fa = mutextextract_main }, "extextract", "Generates a .docx file from mudraw XML output" },
 #endif
 #endif
