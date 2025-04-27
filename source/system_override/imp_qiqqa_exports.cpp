de7efcf863ee37d2686fc9ba4eadd2dfae59d610
Ger Hobbelt <ger@hobbelt.com>
2025-04-27 22:26:29 +0200

    fix/tweak for our abort/exit call override library code, which was introduced quite a while ago to help us debugging obnoxious library code which bluntly calls abort() or exit(), which are pretty hard to put breakpoints on in any orderly & repeatable fashion. This fix is for 32-bit builds, which haven't been tested before with this code: only now do we use this in the msvc_sln_cleaner codebase.
    -------------------------------
    
    // only when building in 32-bit mode do we need these as well, as the name mangling in object files is just a tad different then! *sigh*
    
    #if defined(_WIN32) && !defined(_WIN64)
    
    // Warning: adding `_ACRTEXP` here will crash/fault the MSVC linker with a very obscure error message:
    //
    //  2>LINK : the 32-bit linker (C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\bin\HostX86\x86\link.exe) failed to do memory mapped file I/O
    //           on `Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.exp' and is going to restart linking with a 64-bit linker for better throughput
    //  2>LINK : restarting link with 64-bit linker `C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\bin\HostX64\x86\link.exe'
    //  2>       Creating library Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.lib and
    //           object Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.exp
    //  2>Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.exp : fatal error LNK1106: invalid file or disk full: cannot seek to 0x713F054E

--- source/system_override/imp_qiqqa_exports.cpp
+++ source/system_override/imp_qiqqa_exports.cpp
@@ -93,6 +93,41 @@ namespace std {
 }
 
 
+
+
+
+
+
+
+
+
+
+// only when building in 32-bit mode do we need these as well, as the name mangling in object files is just a tad different then! *sigh*
+
+#if defined(_WIN32) && !defined(_WIN64)
+
+// Warning: adding `_ACRTEXP` here will crash/fault the MSVC linker with a very obscure error message:
+//
+//  2>LINK : the 32-bit linker (C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\bin\HostX86\x86\link.exe) failed to do memory mapped file I/O
+//           on `Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.exp' and is going to restart linking with a 64-bit linker for better throughput
+//  2>LINK : restarting link with 64-bit linker `C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\bin\HostX64\x86\link.exe'
+//  2>       Creating library Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.lib and
+//           object Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.exp
+//  2>Z:\lib\tooling\qiqqa\MuPDF\platform\win32\bin\Release-Unicode-32bit-x86\msvc_sln_cleaner.exp : fatal error LNK1106: invalid file or disk full: cannot seek to 0x713F054E
+
+extern "C" /* _ACRTEXP */ __declspec(noreturn) void __cdecl _imp__qiqqa_abort_application(void)
+{
+	invoke_abort();
+}
+
+extern "C" /* _ACRTEXP */ __declspec(noreturn) void __cdecl _imp__qiqqa_exit_application(int _Code)
+{
+	invoke_exit(_Code);
+}
+
+#endif   // defined(_WIN32) && !defined(_WIN64)
+
+
 #endif
 
 
