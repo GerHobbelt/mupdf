dc6a9b8cdeea5cf23aed93df2f33deabfbd16616
Ger Hobbelt <ger@hobbelt.com>
2025-06-15 19:41:38 +0200

    updated/regenerated the boost header files' include symlinks set

---
+++ scripts/boost/include/boost/mqtt5/impl/re_auth_op.hpp
@@ -0,0 +1,10 @@
+//
+// symlink-like file is generated using the platform/win32/prep_boost_include_directories.sh shell script.
+//
+// ** DO NOT EDIT ** (adjust the regenerator shell script mentioned above instead!)
+//
+
+//#pragma once
+
+#include "../../../thirdparty/owemdjee/boost/libs/mqtt5/include/boost/mqtt5/impl/re_auth_op.hpp"
+
