53914f84a9d33487ae0f08cb16f02c16e78015ac
Ger Hobbelt <ger@hobbelt.com>
2025-07-31 21:15:58 +0200

    fixes for googlelog-unittest project/binary.

--- scripts/googlelog-unittest/pch.cpp
+++ scripts/googlelog-unittest/pch.cpp
@@ -4,3 +4,27 @@
 //
 
 #include "pch.h"
+
+
+// ripped from gflags\test\gflags_unittest.cc
+
+namespace google {
+
+	std::ostream & gflags_stderr_ostream() {
+		return std::cerr;
+	}
+
+	void gflags_stderr_printf(const char *msg, ...) {
+		va_list args;
+		va_start(args, msg);
+		vfprintf(stderr, msg, args);
+		va_end(args);
+		fflush(stderr);
+	}
+
+	void gflags_std_exit(int retval) {
+		::std::exit(retval);
+	}
+
+}		// namespace GFLAGS_NAMESPACE
+
