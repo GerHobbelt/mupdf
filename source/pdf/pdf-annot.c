2e822d3cdf33cf7adfe553ea280e2ad39ab01391
Robin Watts <Robin.Watts@artifex.com>
2026-01-22 14:05:39 +0000

    Bug 709077: Fix missing end operation call in pdf_set_annot_callout_style.
    Update various bits of code in the same file to follow the same
    pattern; consistently set dirty_annot after the try.
    
    # Conflicts:
    #	source/pdf/pdf-annot.c

--- source/pdf/pdf-annot.c
+++ source/pdf/pdf-annot.c
@@ -1267,8 +1267,6 @@ pdf_set_annot_rect(fz_context *ctx, pdf_annot *annot, fz_rect rect)
 		{
 			pdf_set_annot_callout_point(ctx, annot, pdf_annot_callout_point(ctx, annot));
 		}
-
-		pdf_dirty_annot(ctx, annot);
 	}
 	fz_always(ctx)
 	{
@@ -1279,6 +1277,8 @@ pdf_set_annot_rect(fz_context *ctx, pdf_annot *annot, fz_rect rect)
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 const char *
@@ -1320,6 +1320,8 @@ pdf_set_annot_contents(fz_context *ctx, pdf_annot *annot, const char *text)
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 static pdf_obj *markup_subtypes[] = {
@@ -1365,7 +1367,6 @@ pdf_set_annot_rich_contents(fz_context *ctx, pdf_annot *annot, const char *plain
 		check_allowed_subtypes(ctx, annot, PDF_NAME(RC), markup_subtypes);
 		pdf_dict_put_text_string(ctx, annot->obj, PDF_NAME(Contents), plain);
 		pdf_dict_put_text_string(ctx, annot->obj, PDF_NAME(RC), rich);
-		pdf_dirty_annot(ctx, annot);
 		end_annot_op(ctx, annot);
 	}
 	fz_catch(ctx)
@@ -1373,6 +1374,8 @@ pdf_set_annot_rich_contents(fz_context *ctx, pdf_annot *annot, const char *plain
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 static pdf_obj *default_style_subtypes[] = {
@@ -1402,7 +1405,6 @@ pdf_set_annot_rich_defaults(fz_context *ctx, pdf_annot *annot, const char *style
 	{
 		check_allowed_subtypes(ctx, annot, PDF_NAME(DS), default_style_subtypes);
 		pdf_dict_put_text_string(ctx, annot->obj, PDF_NAME(DS), style);
-		pdf_dirty_annot(ctx, annot);
 		end_annot_op(ctx, annot);
 	}
 	fz_catch(ctx)
@@ -1410,6 +1412,8 @@ pdf_set_annot_rich_defaults(fz_context *ctx, pdf_annot *annot, const char *style
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 int
@@ -2568,6 +2572,8 @@ pdf_set_annot_color(fz_context *ctx, pdf_annot *annot, int n, const float *color
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 static pdf_obj *interior_color_subtypes[] = {
@@ -2615,6 +2621,8 @@ pdf_set_annot_interior_color(fz_context *ctx, pdf_annot *annot, int n, const flo
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 static pdf_obj *line_subtypes[] = {
@@ -3069,6 +3077,8 @@ void pdf_set_annot_vertex(fz_context *ctx, pdf_annot *annot, int i, fz_point p)
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 static pdf_obj *quad_point_subtypes[] = {
@@ -3691,6 +3701,8 @@ pdf_set_annot_reply_type(fz_context *ctx, pdf_annot *annot, int rt)
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 static pdf_obj *intent_subtypes[] = {
@@ -3847,6 +3859,8 @@ pdf_set_annot_intent(fz_context *ctx, pdf_annot *annot, enum pdf_intent it)
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 static pdf_obj *callout_subtypes[] = {
@@ -3885,6 +3899,7 @@ void pdf_set_annot_callout_style(fz_context *ctx, pdf_annot *annot, enum pdf_lin
 	{
 		check_allowed_subtypes(ctx, annot, PDF_NAME(CL), callout_subtypes);
 		pdf_dict_put(ctx, annot->obj, PDF_NAME(LE), pdf_name_from_line_ending(ctx, style));
+		end_annot_op(ctx, annot);
 	}
 	fz_catch(ctx)
 	{
@@ -3968,7 +3983,6 @@ void pdf_set_annot_callout_line(fz_context *ctx, pdf_annot *annot, fz_point call
 			pdf_dict_del(ctx, annot->obj, PDF_NAME(CL));
 		}
 
-		pdf_dirty_annot(ctx, annot);
 		end_annot_op(ctx, annot);
 	}
 	fz_catch(ctx)
@@ -3976,6 +3990,8 @@ void pdf_set_annot_callout_line(fz_context *ctx, pdf_annot *annot, fz_point call
 		abandon_annot_op(ctx, annot);
 		fz_rethrow(ctx);
 	}
+
+	pdf_dirty_annot(ctx, annot);
 }
 
 fz_point pdf_annot_callout_point(fz_context *ctx, pdf_annot *annot)
